5d55ac56f210d77eebe784e099e4ce4be2493fb2
Ricky Martinez <RMartinezW16@gmail.com>
2022-07-24 19:11:50 -0400

    plusMinus

---
+++ plusMinusSolution.js
@@ -0,0 +1,61 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'plusMinus' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+
+// //helper function
+// function percentage(partialValue, totalValue) {
+//    return (partialValue) / totalValue;
+// } 
+
+function plusMinus(arr) {
+    // Write your code here
+    const arrayLength = arr.length;
+    let negativeArray = [];
+    let positiveArray = [];
+    let zeroArray = [];
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] < 0){
+            negativeArray.push(arr[i]);
+        } else if ( arr[i] > 0 ){
+            positiveArray.push(arr[i])
+        } else {
+            zeroArray.push(arr[i]);
+        }
+    }
+    console.log((positiveArray.length / arrayLength).toFixed(6));
+    console.log((negativeArray.length / arrayLength).toFixed(6));
+    console.log((zeroArray.length / arrayLength).toFixed(6))
+}
+
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    plusMinus(arr);
+}
