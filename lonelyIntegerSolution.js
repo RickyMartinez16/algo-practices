ba911b9de67edc7a44143702883367f6b7a03b6a
Ricky Martinez <RMartinezW16@gmail.com>
2022-07-26 12:13:25 -0400

    lonely int

--- lonelyIntegerSolution.js
+++ lonelyIntegerSolution.js
@@ -1,12 +1,15 @@
 function lonelyinteger(a) {
-    let sum =0;
-    let sumx = 0;
-    a.forEach(item => {
-        sum += item
+    let sum1 = 0;  //set var of sum1 0
+    let sum2 = 0;  //set var of sum2 to 0
+    a.forEach(int => {    //for each int in the input array sum them to sum1
+        sum1 += int
     })
-    let uniq = new Set(a);
-    uniq.forEach(x => {
-        sumx += x
+    let uniq = new Set(a);    //make a new unique set that only cotains unique ints from the input array
+    uniq.forEach(x => {     //for each X in the unique array add them to sum2
+        sum2 += x
     })
-    return 2*sumx - sum;
+    return 2 * sum2 - sum1;     // return 2 times sum2 - sum1
 }
+
+//input: a = [1, 2, 3, 2, 1]
+//output = 3
