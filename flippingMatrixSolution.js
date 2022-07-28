a455022edbb5dab374f4831c0aca2756797bebbb
Ricky Martinez <RMartinezW16@gmail.com>
2022-07-28 16:54:30 -0400

    flippingMatrix

---
+++ flippingMatrixSolution.js
@@ -0,0 +1,21 @@
+function flippingMatrix(matrix) {
+    let m = matrix
+    let l = matrix.length - 1
+    let sm = Math.floor(matrix.length/2)
+    let total = 0;
+    for (let i = 0; i < sm; i++) {
+        for (let j = 0; j < sm; j++) {
+            total += Math.max(m[i][j], m[l-i][j], m[i][l-j], m[l-i][l-j])
+        }
+    }
+    return total
+}
+
+// 1
+// 2
+// 112 42 83 119
+// 56 125 56 49
+// 15 78 101 43
+// 62 98 114 108
+
+//output: 414
