c116df7bff70a510afad3ba0dec260811ba1c588
Ricky Martinez <RMartinezW16@gmail.com>
2022-08-08 13:00:57 -0400

    sales by match comments

--- salesByMatchSolution.js
+++ salesByMatchSolution.js
@@ -1,13 +1,14 @@
 function sockMerchant(n, ar) {
-  let pairs = 0;
-  const maps = new Map();
-  for (let i = 0; i < ar.length; i++) {
-    if (maps.has(ar[i])) {
-      maps.delete(ar[i]);
-      pairs++;
+  let pairs = 0;  //set counter for pairs
+
+  const maps = new Map(); // make a new empty map
+  for (let i = 0; i < ar.length; i++) {    //loop thru the array
+    if (maps.has(ar[i])) {    //if the map has the number already
+      maps.delete(ar[i]);    //delete the number
+      pairs++;                 // and had one to the pairs counter
     } else {
-      maps.set(ar[i]);
+      maps.set(ar[i]);   //if the map doesnt have the number a lready then add it in the map
     }
   }
-  return pairs;
+  return pairs;        // return the total number of pairs
 }
