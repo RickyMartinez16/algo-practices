a3337c704ae436585190284ae3d737b37af63278
Ricky Martinez <RMartinezW16@gmail.com>
2022-08-11 16:59:33 -0400

    perfect sq comments

--- nextPerfectSquareSolution.js
+++ nextPerfectSquareSolution.js
@@ -1,8 +1,8 @@
 function findNextSquare(sq) {
-  let inputSq = Math.sqrt(sq);
-  if (inputSq % 1 !== 0) {
-    return -1;
+  let inputSq = Math.sqrt(sq);   //find the square root of the input
+  if (inputSq % 1 !== 0) {         //if the inputSq is NOT a full number return -1
+    return -1;      
   }
-  let newSquare = inputSq + 1;
-  return newSquare * newSquare;
+  let newSquare = inputSq + 1;      //new sqaure + 1
+  return newSquare * newSquare;     //multiply to get new perfect square
 }
