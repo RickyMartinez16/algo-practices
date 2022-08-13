0e7bf65679a7179addd6166fbedd815aeb20bb94
Ricky Martinez <RMartinezW16@gmail.com>
2022-08-13 19:17:16 -0400

    vowel count

--- vowelCountSolution.js
+++ vowelCountSolution.js
@@ -1,4 +1,4 @@
 function getCount(str) {
-    let count = str.match(/[aeiou]/gi);
-    return count === null ? 0 : count.length;
+    let count = str.match(/[aeiou]/gi);     //  .match returns an array. returns an array that contains any vowels
+    return count === null ? 0 : count.length;      //if count is null return 0 if not return count.length 
 }
