f299425e75d3636416aced61eaa106f2ae76c8f4
Ricky Martinez <RMartinezW16@gmail.com>
2022-08-14 17:08:17 -0400

    braces comments

--- validBracesSolution.js
+++ validBracesSolution.js
@@ -4,11 +4,11 @@ function validBraces(braces) {
     braces
       .split("")        //split each brace into a new array    
       .map((brace) => {     //map over the new brace array
-        if ("({[".indexOf(brace) >= 0) {       //
-          stack.push(brace);
-          return true;
-        } else {
-          let e = stack.pop();
+        if ("({[".indexOf(brace) >= 0) {       // if the index of brace is in "({[" it will return 0 or greater. if not it will return -1
+          stack.push(brace);    //if it is true then push that brace onto the map
+          return true;      //return true
+        } else {    //else if it isnt in "({[" it will return -1
+          let e = stack.pop();      // new variable e =  the pop() of the stack. so the last one on the stack.
           return "({[".indexOf(e) == ")}]".indexOf(brace);
         }
       })
