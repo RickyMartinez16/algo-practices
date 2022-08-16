c31f3cb05f79e3cdbe6f7571b4cae0f60e41532c
Ricky Martinez <RMartinezW16@gmail.com>
2022-08-16 19:45:05 -0400

    word order

--- yourOrderPleaseSolution.js
+++ yourOrderPleaseSolution.js
@@ -1,6 +1,8 @@
-
-function order(words){
-    return words.split(' ').sort(function(a, b){        //split the word input into an array then sort them
-        return a.match(/\d/) - b.match(/\d/);           //use regex to find a digit and then sort the words
-     }).join(' ');                 // join the the array in the new order .join return a string
-  } 
+function order(words) {
+  return words
+    .split(" ")       //split the word input into an array then sort them
+    .sort(function (a, b) {
+      return a.match(/\d/) - b.match(/\d/); //use regex to find a digit and then sort the words
+    })
+    .join(" "); // join the the array in the new order .join return a string
+}
