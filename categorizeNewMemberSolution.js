7e3092c69c41e8f04a1458260d6b85592ce2d10b
Ricky Martinez <RMartinezW16@gmail.com>
2022-08-31 22:17:37 -0400

    best prac

--- categorizeNewMemberSolution.js
+++ categorizeNewMemberSolution.js
@@ -9,3 +9,13 @@ function openOrSenior(data){
     }  
     return array        //return array
   }
+
+
+  // ----------- best practice below -------- //
+
+// Destructuring: [age, handicap] https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+// Arrow Functions: () => {} https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+
+function openOrSenior(data){
+    return data.map(([age, handicap]) => (age > 54 && handicap > 7) ? 'Senior' : 'Open');
+  }
