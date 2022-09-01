bb6592cdc8b638aa810981af873f8e0b9236dc35
Ricky Martinez <RMartinezW16@gmail.com>
2022-09-01 12:33:45 -0400

    comments

--- sumOfNumbersSolution.js
+++ sumOfNumbersSolution.js
@@ -1,9 +1,16 @@
 function getSum(a, b) {
-  if (a == b) {
-    return a;
-  } else if (a < b) {
-    return a + getSum(a + 1, b);
-  } else {
-    return a + getSum(a - 1, b);
+  if (a == b) {     //if a is the same as b 
+    return a;       //return one of them
+  } else if (a < b) {       //else if a is less than b
+    return a + getSum(a + 1, b);        //recursively call a plus getSum with a now being a + 1 and then it checks again
+  } else {      //else if a is greater than b 
+    return a + getSum(a - 1, b);        //recursively call a minus getSum with a now being a - 1
   }
 }
+
+
+// getSum(3, 6)
+
+// 3 + 4, 6
+// 3 + 5, 6
+// 3 + 6, 6
