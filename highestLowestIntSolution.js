976f97db9e827119af505117fa8e57056dcfe16c
Ricky Martinez <RMartinezW16@gmail.com>
2022-09-01 11:12:35 -0400

    alternate solution

--- highestLowestIntSolution.js
+++ highestLowestIntSolution.js
@@ -1,10 +1,16 @@
-function highAndLow(numbers){
-    let sortedNumbers = numbers.split(" ").sort((a, b) => a - b);
-    if(sortedNumbers.length === 1){
-      return numbers +" "+ numbers
-    } else {
-        let first = sortedNumbers.pop()
-        let second = sortedNumbers.shift()
-        return (first +" "+ second)
-    }
+function highAndLow(numbers) {
+  let sortedNumbers = numbers.split(" ").sort((a, b) => a - b);
+  if (sortedNumbers.length === 1) {
+    return numbers + " " + numbers;
+  } else {
+    let first = sortedNumbers.pop();
+    let second = sortedNumbers.shift();
+    return first + " " + second;
   }
+}
+
+//------alternate soltuion -------//
+function highAndLow(numbers) {
+  numbers = numbers.split(" ");
+  return `${Math.max(...numbers)} ${Math.min(...numbers)}`;
+}
