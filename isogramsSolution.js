314be085f5bc05815c733bf3fbca6dc2ba0a9f07
Ricky Martinez <RMartinezW16@gmail.com>
2022-09-06 12:30:37 -0400

    isogram comments

--- isogramsSolution.js
+++ isogramsSolution.js
@@ -1,11 +1,14 @@
-function isIsogram(str){
-    let lowerCaseString = str.toLowerCase();
-    let lowerCaseStringArray = lowerCaseString.split("");
-    
-    for(let i = 0; i < lowerCaseStringArray.length; i++){
-      if(lowerCaseStringArray.indexOf(lowerCaseStringArray[i]) !== lowerCaseStringArray.lastIndexOf(lowerCaseStringArray[i])){
-        return false
-      }
+function isIsogram(str) {
+  let lowerCaseString = str.toLowerCase();      //ignore case so make string lowercase
+  let lowerCaseStringArray = lowerCaseString.split("");     //split the string into an array
+
+  for (let i = 0; i < lowerCaseStringArray.length; i++) {       //loop thru array
+    if (
+      lowerCaseStringArray.indexOf(lowerCaseStringArray[i]) !==     //if the indexOf the array does not equal the last index of the same spot its false and repeats
+      lowerCaseStringArray.lastIndexOf(lowerCaseStringArray[i])
+    ) {
+      return false;
     }
-    return true
   }
+  return true;      //if it completes check its true 
+}
