f702218444a524fc4b256bf3bc6a6e6553abcb6d
Ricky Martinez <RMartinezW16@gmail.com>
2022-09-06 12:40:20 -0400

    more best prac comments

--- isogramsSolution.js
+++ isogramsSolution.js
@@ -26,6 +26,26 @@ function isIsogram(str){
     return new Set(str.toUpperCase()).size == str.length;
   }
 
-function isIsogram(str){ 
+
+
+  function isIsogram(str){ 
     return !/(\w).*\1/i.test(str)
   }
+
+//the regex test method validates a string against a given expression. In this case: /(\w).*\1/i
+
+// To break it down:
+
+// The first set of parenthesis is to define a capturing group. Within that group, 
+// there is \w - this is to match any word character (basically a-zA-Z0-9_).
+// After that, there is a period followed by an asterisk. The period signifies any character at all - 
+// whether a letter, number, space, symbol etc. Usually, the engine will stop looking once a 
+// single character is found, however, adding the asterisk flag makes the search both optional AND greedy - 
+// meaning the engine will keep matching the following character until something doesn't match or the 
+// end of the string is reached. The point of this is that there may be other characters inbetween duplicate characters.
+// The next part is \1 - this is where the real magic happens. A backslash followed by a number is 
+// used to point to the capture group defined earlier (in this case, (\w). Essentially, the engine 
+// treats the first matched letter as a variable, and looks for the exact same variable again. If the string 
+// only has unique characters, nothing will match because it is explicitly looking for the same character twice.
+// The test method returns either true or false if the expression matches something. Since the expression is 
+// looking for an instance of a repeating character, and the kata is to test the reverse, we simply invert the resulting boolean.
