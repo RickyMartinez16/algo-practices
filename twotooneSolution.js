64ba61b9532bc23e6680ddb3862652c88b6156ce
Ricky Martinez <RMartinezW16@gmail.com>
2022-09-06 19:37:44 -0400

    best prac

--- twotooneSolution.js
+++ twotooneSolution.js
@@ -1,5 +1,10 @@
 function longest(s1, s2) {
-    let uniqueLetters = [...new Set(s1), ...new Set(s2)]
-    let finalArray = [...new Set(uniqueLetters)].sort()
-    return finalArray.join("")
-  }
+  let uniqueLetters = [...new Set(s1), ...new Set(s2)];
+  let finalArray = [...new Set(uniqueLetters)].sort();
+  return finalArray.join("");
+}
+
+
+//----best practice-----//
+
+const longest = (s1, s2) => [...new Set(s1+s2)].sort().join('')
