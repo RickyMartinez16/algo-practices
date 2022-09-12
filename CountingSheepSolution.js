d0803df47b0b9a54b6748268abe28f2b3280996f
Ricky Martinez <RMartinezW16@gmail.com>
2022-09-12 19:57:28 -0400

    my solution comments

--- CountingSheepSolution.js
+++ CountingSheepSolution.js
@@ -1,9 +1,9 @@
 function countSheeps(arrayOfSheep) {
-  let finalArray = [];
-  for (let i = 0; i < arrayOfSheep.length; i++) {
-    if (arrayOfSheep[i] === true) {
+  let finalArray = [];      //set var array empty 
+  for (let i = 0; i < arrayOfSheep.length; i++) {   //for loop thru input array
+    if (arrayOfSheep[i] === true) {     //if index of array is true push it into new array
       finalArray.push(arrayOfSheep[i]);
     }
   }
-  return finalArray.length;
+  return finalArray.length;     //return array length
 }
