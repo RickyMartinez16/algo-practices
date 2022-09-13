4d326ac58480373945c840d205ac75adb8cb579c
Ricky Martinez <RMartinezW16@gmail.com>
2022-09-12 21:54:11 -0400

    abbrviate comments

--- abbreviateTwoNameSolution.js
+++ abbreviateTwoNameSolution.js
@@ -1,4 +1,4 @@
 function abbrevName(name) {
-  let names = name.split(" ");
-  return (names[0].charAt(0) + "." + names[1].charAt(0)).toUpperCase();
+  let names = name.split(" ");      //split the names into an array
+  return (names[0].charAt(0) + "." + names[1].charAt(0)).toUpperCase();     //return the first letter of each array to upper case
 }
