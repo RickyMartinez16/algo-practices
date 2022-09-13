d128dc53806f88efdcfd9cc9653f2e580333ff0b
Ricky Martinez <RMartinezW16@gmail.com>
2022-09-12 20:03:56 -0400

    my solution commets

--- oppositesAttractSolution.js
+++ oppositesAttractSolution.js
@@ -1,5 +1,5 @@
 function lovefunc(flower1, flower2) {
-  if ((flower1 + flower2) % 2 != 0) {
+  if ((flower1 + flower2) % 2 != 0) {   //if flower one plus flower 2 remainder does not equal 0 its an odd number
     return true;
   } else {
     return false;
