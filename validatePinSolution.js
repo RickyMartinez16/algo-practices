5dc70edae015ce31f74541bf411f82bf81aa9e14
Ricky Martinez <RMartinezW16@gmail.com>
2022-09-13 20:12:32 -0400

    best prac

--- validatePinSolution.js
+++ validatePinSolution.js
@@ -1,6 +1,11 @@
 function validatePIN(pin) {
-    var isNumber = /^\d+$/.test(pin) && (pin.length == 4 || pin.length == 6)
-    return isNumber
-    }
-    
-    validatePIN('0193')
+  var isNumber = /^\d+$/.test(pin) && (pin.length == 4 || pin.length == 6);
+  return isNumber;
+}
+
+
+//-------best prac------//
+
+function validatePIN(pin) {
+    return /^(\d{4}|\d{6})$/.test(pin)
+  }
