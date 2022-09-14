187fd4a4dc01fe0dcc0be7ba0f0f4a972c5ad51f
Ricky Martinez <RMartinezW16@gmail.com>
2022-09-14 15:26:27 -0400

    my solution comments

--- shortestWordLengthSolution.js
+++ shortestWordLengthSolution.js
@@ -1,12 +1,12 @@
 function findShort(s) {
-  let wordSplitArray = s.split(" ");
+  let wordSplitArray = s.split(" ");       //split the array into indivdual workds
 
-  let shortestWordLength = Infinity;
+  let shortestWordLength = Infinity;    //make a variable a really high numnber
 
-  for (let i = 0; i < wordSplitArray.length; i++) {
-    if (wordSplitArray[i].length < shortestWordLength) {
-      shortestWordLength = wordSplitArray[i].length;
+  for (let i = 0; i < wordSplitArray.length; i++) {     //for loop to check each word in the split array
+    if (wordSplitArray[i].length < shortestWordLength) {    //if the current word is less than shortestWord var
+      shortestWordLength = wordSplitArray[i].length;    // change the shortest word var to that word
     }
   }
-  return shortestWordLength;
+  return shortestWordLength;    //return shortest word var
 }
