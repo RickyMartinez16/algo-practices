b37072934ba0beb7e9a55ce698aff8af0426b2a5
Ricky Martinez <RMartinezW16@gmail.com>
2022-09-14 14:15:46 -0400

    comments

--- classPointsAverageSolution.js
+++ classPointsAverageSolution.js
@@ -3,11 +3,11 @@ function betterThanAverage(classPoints, yourPoints) {
     let classTotalPoints = classPoints.reduce(
     (previousValue, currentValue) => previousValue + currentValue,
     0
-  );
-  let average = classTotalPoints / classPoints.length
-  if(yourPoints > average){
-    return true
-  } else {
+  );        //add all the class points together to get one value using .reduce array method
+  let average = classTotalPoints / classPoints.length   //calculayte the average dividing total by how many students 
+  if(yourPoints > average){     //check if your points is greater than average then return true
+    return true 
+  } else {      //if not return false
     return false
   }
 }
