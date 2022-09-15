52e3f1abc1cd2a7f53cf618d4c9d41e18bf7d46d
Ricky Martinez <RMartinezW16@gmail.com>
2022-09-15 10:44:38 -0400

    comments

--- reverseStringSolution.js
+++ reverseStringSolution.js
@@ -1,3 +1,3 @@
 function solution(str){
-    return str.split("").reverse().join("");
+    return str.split("").reverse().join("");  //split the string into a new array then reverse the new array then join the new array to make a new string
   }
