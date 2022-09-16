850bc31ada7be91e28e7700c66834cd016d3fe2d
Ricky Martinez <RMartinezW16@gmail.com>
2022-09-16 14:39:52 -0400

    banjo comments

--- areYouPlayingBanjoSolution.js
+++ areYouPlayingBanjoSolution.js
@@ -1,8 +1,8 @@
 function areYouPlayingBanjo(name) {
-  let lowerCaseName = name.toLowerCase();
-  if (lowerCaseName.charAt(0) === "r") {
-    return name + " plays banjo";
+  let lowerCaseName = name.toLowerCase();    //make the name all lowercase who cares about case
+  if (lowerCaseName.charAt(0) === "r") {    //check the first letter in the name
+    return name + " plays banjo";       //if its "r" return the playing nbanjo
   } else {
-    return name + " does not play banjo";
+    return name + " does not play banjo";       //if not return not playing
   }
 }
