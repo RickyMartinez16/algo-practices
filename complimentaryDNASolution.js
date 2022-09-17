30be39aeae1926f3b8369729269656e57ffc791b
Ricky Martinez <RMartinezW16@gmail.com>
2022-09-17 16:12:03 -0400

    dna comments

--- complimentaryDNASolution.js
+++ complimentaryDNASolution.js
@@ -1,15 +1,15 @@
-function DNAStrand(dna){
-    let newDNA = [];
-    for(let i = 0; i < dna.length; i++){
-      if(dna[i] === "A"){
-        newDNA.push("T")
-      } else if(dna[i] === "T"){
-        newDNA.push("A")
-      } else if(dna[i] === "G"){
-        newDNA.push("C")
-      } else if(dna[i] === "C"){
-        newDNA.push("G")
-      }
+function DNAStrand(dna) {
+  let newDNA = [];  //make new dna array
+  for (let i = 0; i < dna.length; i++) {    //loop thru the input dna string
+    if (dna[i] === "A") {       //check what each [i] of the dna string is and push the opposite into new array
+      newDNA.push("T");
+    } else if (dna[i] === "T") {
+      newDNA.push("A");
+    } else if (dna[i] === "G") {
+      newDNA.push("C");
+    } else if (dna[i] === "C") {
+      newDNA.push("G");
     }
-    return newDNA.join("")
   }
+  return newDNA.join("");   //return new array joined so its a string
+}
