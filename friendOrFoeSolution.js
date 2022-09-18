85734a6ad274e7c4719c36f13a40de653e7a8e64
Ricky Martinez <RMartinezW16@gmail.com>
2022-09-18 11:55:21 -0400

    firend or foe comments

--- friendOrFoeSolution.js
+++ friendOrFoeSolution.js
@@ -1,9 +1,9 @@
 function friend(friends) {
-  let friendsArray = [];
-  for (let i = 0; i < friends.length; i++) {
-    if (friends[i].length === 4) {
-      friendsArray.push(friends[i]);
+  let friendsArray = [];    //set new empty array
+  for (let i = 0; i < friends.length; i++) {    // loop thru input
+    if (friends[i].length === 4) {  //check each array item if the length equals 4
+      friendsArray.push(friends[i]);    //push it into new array
     }
   }
-  return friendsArray;
+  return friendsArray;  //return final aray
 }
