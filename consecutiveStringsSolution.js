473dc2381841875b1af9fdc8c060f1c06e2ccf29
Ricky Martinez <RMartinezW16@gmail.com>
2022-09-21 13:02:44 -0400

    concat strings best practice

--- consecutiveStringsSolution.js
+++ consecutiveStringsSolution.js
@@ -37,3 +37,17 @@ function longestConsec(strarr, k) {
     return longest;
   }
 }
+
+//-------best prac-------//
+
+function longestConsec(strarr, k) {
+  var longest = "";
+  for (var i = 0; k > 0 && i <= strarr.length - k; i++) {
+    var tempArray = strarr.slice(i, i + k);
+    var tempStr = tempArray.join("");
+    if (tempStr.length > longest.length) {
+      longest = tempStr;
+    }
+  }
+  return longest;
+}
