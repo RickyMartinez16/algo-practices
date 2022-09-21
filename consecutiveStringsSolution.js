16ae6053e814743dc60aa136c8d15495581655d9
Ricky Martinez <RMartinezW16@gmail.com>
2022-09-21 13:01:58 -0400

    working concat strings

--- consecutiveStringsSolution.js
+++ consecutiveStringsSolution.js
@@ -20,4 +20,20 @@ function longestConsec(strarr, k) {
   return finalConcat;
 }
 
+//-------working-----//
 
+function longestConsec(strarr, k) {
+  if (strarr.length == 0 || k > strarr.length || k <= 0) {
+    return "";
+  } else {
+    var list = [];
+    for (var i = 0; i < strarr.length - k + 1; i++) {
+      list[i] = strarr.slice(i, k + i).join("");
+    }
+
+    var longest = list.reduce(function (a, b) {
+      return a.length >= b.length ? a : b;
+    });
+    return longest;
+  }
+}
