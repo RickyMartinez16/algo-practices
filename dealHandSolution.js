c53d8cdd0635dd7aa95c7fa9233d14419532261b
Ricky Martinez <RMartinezW16@gmail.com>
2022-09-21 22:24:34 -0400

    comments

--- dealHandSolution.js
+++ dealHandSolution.js
@@ -1,15 +1,15 @@
 function dealHand(deck, hand_size) {
-  let hand = [];
+  let hand = [];    //ser empty array var
 
-  while (hand.length < hand_size) {
-    let card = getRandomItem(deck);
-    
-    if (!hand.includes(card)) {
-      hand.push(card);
+  while (hand.length < hand_size) {     //while hand length is less than hand size
+    let card = getRandomItem(deck);     //get a random card to card var
+
+    if (!hand.includes(card)) {     //if hand array does not include the card
+      hand.push(card);      //push the card into the array
     }
 
-    if (hand.length === hand_size) {
-      return hand;
+    if (hand.length === hand_size) {        //if the length of the hand array equals the hand size
+      return hand;      //rerun the hand
     }
   }
 }
