9de9fb56866675f5ed2053bc5137d7fca3170b59
Ricky Martinez <RMartinezW16@gmail.com>
2022-09-21 22:20:58 -0400

    deal hand comments

--- dealHandSolution.js
+++ dealHandSolution.js
@@ -1,32 +1,26 @@
-
-
 function dealHand(deck, hand_size) {
-    
   let hand = [];
-    
-    while(hand.length < hand_size){
-      let card = getRandomItem(deck);
-      console.log(card)
-      if(!hand.includes(card)){
-         hand.push(card)
-      }
-   
-      if(hand.length === hand_size){
-        return hand
-      }
+
+  while (hand.length < hand_size) {
+    let card = getRandomItem(deck);
+    console.log(card);
+    if (!hand.includes(card)) {
+      hand.push(card);
     }
-   
+
+    if (hand.length === hand_size) {
+      return hand;
+    }
   }
-  
-  function getRandomItem(deck) {
-  
-      // get random index value
-      const randomIndex = Math.floor(Math.random() * deck.length);
-  
-      // get random item
-      const item = deck[randomIndex];
-    console.log(item)
-  
-      return item;
-    
-  }
+}
+
+function getRandomItem(deck) {
+  // get random index value
+  const randomIndex = Math.floor(Math.random() * deck.length);
+
+  // get random item
+  const item = deck[randomIndex];
+  console.log(item);
+
+  return item;
+}
