5b7d826c5b93d9120b4c1e75dde51b98b4e7bb56
Ricky Martinez <RMartinezW16@gmail.com>
2022-09-22 13:18:55 -0400

    first nth

--- sumOfFirdtNthSolution.js
+++ sumOfFirdtNthSolution.js
@@ -11,3 +11,13 @@ function SeriesSum(n) {
   }
   return result.toFixed(2);
 }
+
+//------best prac-------//
+
+function SeriesSum(n) {
+    for (var s = 0, i = 0; i < n; i++) {
+      s += 1 / (1 + i * 3)
+    }
+    
+    return s.toFixed(2)
+  }
