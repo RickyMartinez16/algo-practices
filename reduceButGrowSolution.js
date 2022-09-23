8bfd991e34bfd893c95e52e60c13929d96eed9d4
Ricky Martinez <RMartinezW16@gmail.com>
2022-09-23 12:13:42 -0400

    reduce but grow comments'

--- reduceButGrowSolution.js
+++ reduceButGrowSolution.js
@@ -1,7 +1,7 @@
 function grow(x) {
-  let sortedArray = x.sort((a, b) => a - b);
+  let sortedArray = x.sort((a, b) => a - b);        //sort the array
   const sumWithInitial = sortedArray.reduce(
-    (previousValue, currentValue) => previousValue * currentValue
+    (previousValue, currentValue) => previousValue * currentValue       //reduce to one value
   );
   return sumWithInitial;
 }
