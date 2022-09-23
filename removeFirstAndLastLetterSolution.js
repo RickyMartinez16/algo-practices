09adf727231c171ac46a197e06e62c18aee32800
Ricky Martinez <RMartinezW16@gmail.com>
2022-09-23 12:38:16 -0400

    first and last comments

--- removeFirstAndLastLetterSolution.js
+++ removeFirstAndLastLetterSolution.js
@@ -1,7 +1,7 @@
 function removeChar(str) {
-  let stringArray = str.split("");
+  let stringArray = str.split("");  //split the string into an array by letter
 
-  let newArray = stringArray.slice(1, stringArray.length - 1);
+  let newArray = stringArray.slice(1, stringArray.length - 1);      //slice the array to remove first and last
 
-  return newArray.join("");
+  return newArray.join(""); //return the string by joining the array
 }
