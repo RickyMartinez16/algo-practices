e778fba3f54fae9ed1911016eb37e3c4ebd8b547
Ricky Martinez <RMartinezW16@gmail.com>
2022-09-24 17:13:08 -0400

    write numbers comments

--- writeNumbersInExpandedFormSolution.js
+++ writeNumbersInExpandedFormSolution.js
@@ -1,15 +1,15 @@
 function expandedForm(num) {
-  var multiple = 10;
-  var result = [];
-  while (num > 1) {
-    var remainder = num % multiple;
-    if (remainder > 0) {
-      result.unshift(remainder);
+  var multiple = 10;        //set variable to the multple of 10
+  var result = [];      //result is an emprt array
+  while (num > 1) {         //while the input num is greater than 1
+    var remainder = num % multiple;        //set the remainder of input num and the multiple of 10
+    if (remainder > 0) {        //if the remainder is greater than 0
+      result.unshift(remainder);        //add remainder to the beginning of the array (unshift)
     }
-    num -= remainder;
-    multiple = multiple * 10;
+    num -= remainder;       //number eqauls number - remainder
+    multiple = multiple * 10;       //multiple equals multiple times 10
   }
-  return result.join(" + ");
+  return result.join(" + ");        //join the array by a + 
 }
 
 //-------best prac------//
