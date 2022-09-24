b5072a91ed07aa6358935c24cb194fd753ff2804
Ricky Martinez <RMartinezW16@gmail.com>
2022-09-24 17:10:10 -0400

    best practice write nunber expanded

--- writeNumbersInExpandedFormSolution.js
+++ writeNumbersInExpandedFormSolution.js
@@ -11,3 +11,15 @@ function expandedForm(num) {
   }
   return result.join(" + ");
 }
+
+//-------best prac------//
+
+const expandedForm = (n) =>
+  n
+    .toString()
+    .split("")
+    .reverse()
+    .map((a, i) => a * Math.pow(10, i))
+    .filter((a) => a > 0)
+    .reverse()
+    .join(" + ");
