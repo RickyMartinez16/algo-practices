007a494872756f30567ba6fe4586dceb40cae64a
Ricky Martinez <RMartinezW16@gmail.com>
2022-09-26 12:27:37 -0400

    comments

--- convertNumberToReverseArraySolution.js
+++ convertNumberToReverseArraySolution.js
@@ -1,8 +1,8 @@
 function digitize(n) {
-  let nString = n.toString();
-  let stringArray = nString.split("").reverse();
-  var numberArray = stringArray.map(Number);
-  return numberArray;
+  let nString = n.toString();       //turned the input number to a string
+  let stringArray = nString.split("").reverse();        //split the string and then reversed the ordeer in the array
+  var numberArray = stringArray.map(Number);      //mapped over the array and made it a number
+  return numberArray; //return the number array
 }
 
 
