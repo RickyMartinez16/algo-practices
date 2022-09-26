ffd46f3d965d1e8ef245392a221afdfafc9c1c7e
Ricky Martinez <RMartinezW16@gmail.com>
2022-09-26 12:23:19 -0400

    reverse number array

--- convertNumberToReverseArraySolution.js
+++ convertNumberToReverseArraySolution.js
@@ -1,7 +1,6 @@
 function digitize(n) {
   let nString = n.toString();
   let stringArray = nString.split("").reverse();
-  console.log(stringArray);
   var numberArray = stringArray.map(Number);
   return numberArray;
 }
