a81fb812dcc89eb9c0fd96a601fd013ee069d560
Ricky Martinez <RMartinezW16@gmail.com>
2022-09-26 11:01:19 -0400

    reverse wods

--- reverseWordsSolution.js
+++ reverseWordsSolution.js
@@ -1,5 +1,5 @@
 function reverseWords(str) {
-    let reverseString = str.split("").reverse().join("")
-    let final = reverseString.split(" ").reverse().join(" ")
-    return final
+    let reverseString = str.split("").reverse().join("")    //split the input string by letter into an array. reverse the array. then join the letters again
+    let final = reverseString.split(" ").reverse().join(" ")    //now each word is reverse byt not the order, so split the words by space, then reverse them, then join them again by sapce
+    return final        //return final string
   }
