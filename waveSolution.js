2d9c56f66c2d4c4606760aa620c8e25da07d8888
Ricky Martinez <RMartinezW16@gmail.com>
2022-09-28 12:51:41 -0400

    best practice wave

--- waveSolution.js
+++ waveSolution.js
@@ -1,12 +1,30 @@
 function wave(str) {
-  let waveArr = [];     //array where wave will go
-  for (let i = 0; i < str.length; i++) {    //loop thru the string
-    let letter = str[i];    //variable each letter
-    if (letter === " ") {    //if the letter is a space continue on in the loop
+  let waveArr = []; //array where wave will go
+  for (let i = 0; i < str.length; i++) {
+    //loop thru the string
+    let letter = str[i]; //variable each letter
+    if (letter === " ") {
+      //if the letter is a space continue on in the loop
       continue;
     } else {
-      waveArr.push(str.slice(0, i) + letter.toUpperCase() + str.slice(i + 1));   //slice to select letters return the uppercase letter and add the sliced end
+      waveArr.push(str.slice(0, i) + letter.toUpperCase() + str.slice(i + 1)); //slice to select letters return the uppercase letter and add the sliced end
     }
   }
-  return waveArr;       //return final arrray
+  return waveArr; //return final arrray
+}
+
+//------best prac------//
+
+function wave(str) {
+  let result = [];
+
+  str.split("").forEach((char, index) => {
+    if (/[a-z]/.test(char)) {
+      result.push(
+        str.slice(0, index) + char.toUpperCase() + str.slice(index + 1)
+      );
+    }
+  });
+
+  return result;
 }
