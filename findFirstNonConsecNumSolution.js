6c88507f23e9cc7a33596fc633a7829ec38d2906
Ricky Martinez <RMartinezW16@gmail.com>
2022-09-28 13:41:56 -0400

    find first non consec num

---
+++ findFirstNonConsecNumSolution.js
@@ -0,0 +1,29 @@
+// function firstNonConsecutive (arr) {
+//   for(let i = 0; i < arr.length; i++){
+//     let number = arr[i];
+//     if(number + 1 !== arr[i + 1]){
+//       return arr[i + 1]
+//     }
+// //     if(number + 1 == arr[i + 1]){
+// //       return null
+// //     }
+//   }
+// }
+
+function firstNonConsecutive (arr) {
+    // Initialize default value to increment
+    let previous = arr[0];
+    // Establish a variable to collect the non consecutive integer
+    let first;
+    for (let i = 1; i < arr.length; i++) {
+      // If prev value is not equal to the next element array val
+      if ((previous + 1) !== arr[i]) {
+        // You've found your first non consecutive and break
+        first = arr[i];
+        break;
+      }
+      previous ++;
+    }
+    // return null if first is never set
+    return first !== undefined ? first : null;
+  }
