29548f1703038e3d2148aadf30172e15a078bc95
Ricky Martinez <RMartinezW16@gmail.com>
2022-09-28 12:45:51 -0400

    comments

--- waveSolution.js
+++ waveSolution.js
@@ -1,12 +1,12 @@
 function wave(str) {
-  let waveArr = [];
-  for (let i = 0; i < str.length; i++) {
-    let letter = str[i];
-    if (letter === " ") {
+  let waveArr = [];     //array where wave will go
+  for (let i = 0; i < str.length; i++) {    //loop thru the string
+    let letter = str[i];    //variable each letter
+    if (letter === " ") {    //if the letter is a space continue on in the loop
       continue;
     } else {
-      waveArr.push(str.slice(0, i) + letter.toUpperCase() + str.slice(i + 1));
+      waveArr.push(str.slice(0, i) + letter.toUpperCase() + str.slice(i + 1));   //slice to select letters return the uppercase letter and add the sliced end
     }
   }
-  return waveArr;
+  return waveArr;       //return final arrray
 }
