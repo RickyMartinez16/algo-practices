d5a758f84aa4333108d54de56305950861d5c407
Ricky Martinez <RMartinezW16@gmail.com>
2022-09-30 12:18:04 -0400

    best poractice

--- arrayDiffSolution.js
+++ arrayDiffSolution.js
@@ -1,11 +1,20 @@
 function arrayDiff(a, b) {
-  for (let i = 0; i < a.length; i++) {      //loop thru the first array
-    for (let j = 0; j < b.length; j++) {        //loop thru the second array to cmopare
-      if (a[i] === b[j]) {      //if a[i] is the same as b[j]
-        a.splice(i, 1);     //splice out that one item 
-        i--;        //make index one less to check again for duplicates
+  for (let i = 0; i < a.length; i++) {
+    //loop thru the first array
+    for (let j = 0; j < b.length; j++) {
+      //loop thru the second array to cmopare
+      if (a[i] === b[j]) {
+        //if a[i] is the same as b[j]
+        a.splice(i, 1); //splice out that one item
+        i--; //make index one less to check again for duplicates
       }
     }
   }
-  return a;     //return the final a array
-}   
+  return a; //return the final a array
+}
+
+//----- best practice -----//
+
+function array_diff(a, b) {
+  return a.filter((e) => !b.includes(e));
+}
