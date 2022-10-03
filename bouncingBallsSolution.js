e592887876c4700dca5f49a17b37cbad449c5557
Ricky Martinez <RMartinezW16@gmail.com>
2022-10-03 14:36:04 -0400

    bouncing balls best prac

--- bouncingBallsSolution.js
+++ bouncingBallsSolution.js
@@ -11,3 +11,19 @@ function bouncingBall(h,  bounce,  window) {
         return -1;
     }
 }
+
+
+//-----best prac----//
+
+function bouncingBall(h,  bounce,  window) {
+    if( h <= 0 || bounce >= 1 || bounce <= 0 || window >= h) return -1
+    let seen = 0;
+    
+    while(h > window){
+      seen += 1
+      h *= bounce
+      if(h > window) seen += 1
+    }
+    
+    return seen;
+  }
