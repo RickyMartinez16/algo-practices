f42f6f1de3038792e9434cf9c762254616b35bba
Ricky Martinez <RMartinezW16@gmail.com>
2022-10-03 15:43:23 -0400

    best practice

--- beginnerPaperWorkSolution.js
+++ beginnerPaperWorkSolution.js
@@ -1,6 +1,13 @@
 function paperwork(n, m) {
-    if(n < 0 || m < 0){
+    if(n < 0 || m < 0){     //check the condition
       return 0
     }
-    return n * m
+    return n * m        
+  }
+
+
+  //----best practice-----//
+
+  function paperwork(n, m) {
+    return n > 0 && m > 0 ? n * m : 0       //ternery
   }
