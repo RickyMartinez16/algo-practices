694f00571e2fda8c0bb4a25002e1474911eafe5e
Ricky Martinez <RMartinezW16@gmail.com>
2022-10-04 17:21:05 -0400

    alphabet condition

---
+++ alphabetConditionSolution.js
@@ -0,0 +1,13 @@
+//not working//
+
+function alphabetPosition(text) {
+    let cleanText = text.replace(/[^a-zA-Z]/g,"").toLowerCase().split("")
+    console.log(cleanText)
+    let numberArray = [];
+    let n 
+    for(let i = 0; i < cleanText.length; i++){
+      n = cleanText.charCodeAt(i) - 97 + 1
+      numberArray.push(n)
+    }
+    return numberArray.join(" ")
+  }
