daa7c3b878480bb6254179801d79ebdb225bf635
Ricky Martinez <RMartinezW16@gmail.com>
2022-10-03 20:18:37 -0400

    comments

--- sumWithoutHighestAndLowestSolution.js
+++ sumWithoutHighestAndLowestSolution.js
@@ -2,7 +2,7 @@ function sumArray(array) {
   if (!array) {     //check condition if array is empty return 0
     return 0;
   }
-  let sortedArray = array.sort((a, b) => a - b);    //sort the array
+  let sortedArray = array.sort((a, b) => a - b);    //sort the array to a var
   
   let newArray = sortedArray.slice(1, sortedArray.length - 1);      //create new array without first and last number
   const sumWithInitial = newArray.reduce(               //reduce the array to one number
