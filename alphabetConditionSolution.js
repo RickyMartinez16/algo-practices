9a90e71def6fdd53b0e3aca3ec4cce611f291671
Ricky Martinez <RMartinezW16@gmail.com>
2022-10-04 17:21:22 -0400

    format

--- alphabetConditionSolution.js
+++ alphabetConditionSolution.js
@@ -1,13 +1,16 @@
 //not working//
 
 function alphabetPosition(text) {
-    let cleanText = text.replace(/[^a-zA-Z]/g,"").toLowerCase().split("")
-    console.log(cleanText)
-    let numberArray = [];
-    let n 
-    for(let i = 0; i < cleanText.length; i++){
-      n = cleanText.charCodeAt(i) - 97 + 1
-      numberArray.push(n)
-    }
-    return numberArray.join(" ")
+  let cleanText = text
+    .replace(/[^a-zA-Z]/g, "")
+    .toLowerCase()
+    .split("");
+  console.log(cleanText);
+  let numberArray = [];
+  let n;
+  for (let i = 0; i < cleanText.length; i++) {
+    n = cleanText.charCodeAt(i) - 97 + 1;
+    numberArray.push(n);
   }
+  return numberArray.join(" ");
+}
