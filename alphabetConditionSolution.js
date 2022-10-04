034b8d51939bc85a76c86b409f64d3944b196533
Ricky Martinez <RMartinezW16@gmail.com>
2022-10-04 17:26:18 -0400

    working solution

--- alphabetConditionSolution.js
+++ alphabetConditionSolution.js
@@ -18,16 +18,19 @@ function alphabetPosition(text) {
 //-------working--------//
 
 function alphabetPosition(text) {
-  let cleanText = text
-    .replace(/[^a-zA-Z]/g, "")
-    .toLowerCase()
-    .split("");
-  console.log(cleanText);
-  let numberArray = [];
-  let n;
-  for (let i = 0; i < cleanText.length; i++) {
-    n = cleanText.charCodeAt(i) - 97 + 1;
-    numberArray.push(n);
+  let alphabet = "abcdefghijklmnopqrstuvwxyz";
+  let alphaNumbers = [];
+
+  text = text.toLowerCase();
+
+  for (let i = 0; i < text.length; i++) {
+    let index = alphabet.indexOf(text[i]);
+
+    if (index === -1) {
+      continue;
+    } else {
+      alphaNumbers.push(index + 1);
+    }
   }
-  return numberArray.join(" ");
+  return alphaNumbers.join(" ");
 }
