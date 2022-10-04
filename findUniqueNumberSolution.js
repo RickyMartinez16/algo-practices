5302f1005b915d48e808630038eeb34703ed33e8
Ricky Martinez <RMartinezW16@gmail.com>
2022-10-04 16:52:49 -0400

    comments uniqye number

--- findUniqueNumberSolution.js
+++ findUniqueNumberSolution.js
@@ -1,4 +1,6 @@
 function findUniq(arr) {
-  arr.sort((a, b) => a - b);
-  return arr[0] == arr[1] ? arr.pop() : arr[0];
+  arr.sort((a, b) => a - b);        //sort the array
+  return arr[0] == arr[1] ? arr.pop() : arr[0];     //use ternery to determine what to return
+                                            //if arr[0] equals arr[1] return arr.pop() (the last in the array)
+                                            //if not return arr[0]
 }
