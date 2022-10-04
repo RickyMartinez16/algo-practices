13b7aa20862e30179a621449807686b6fcca4fb8
Ricky Martinez <RMartinezW16@gmail.com>
2022-10-04 16:50:52 -0400

    formatting

--- findUniqueNumberSolution.js
+++ findUniqueNumberSolution.js
@@ -1,5 +1,4 @@
 function findUniq(arr) {
-    arr.sort((a,b)=>a-b);
-    return arr[0]==arr[1] ? arr.pop() : arr[0]
-  }
-  
+  arr.sort((a, b) => a - b);
+  return arr[0] == arr[1] ? arr.pop() : arr[0];
+}
