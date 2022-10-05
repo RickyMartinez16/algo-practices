e10eba9d920db21d4383ea6fe97abe35c0493aa8
Ricky Martinez <RMartinezW16@gmail.com>
2022-10-05 12:42:02 -0400

    every other comments

--- removeEveryOtherSolution.js
+++ removeEveryOtherSolution.js
@@ -1,8 +1,8 @@
 function removeEveryOther(arr) {
-  let finalArray = [];
+  let finalArray = [];      //empty array var
 
-  for (let i = 0; i < arr.length; i = i + 2) {
-    finalArray.push(arr[i]);
+  for (let i = 0; i < arr.length; i = i + 2) {  //loop thru the array and skip every other
+    finalArray.push(arr[i]);        //push every other into final array 
   }
   return finalArray;
 }
