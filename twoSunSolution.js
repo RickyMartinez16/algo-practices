e127b0e484b3888e56adb258e36032cac739ff48
Ricky Martinez <RMartinezW16@gmail.com>
2022-10-05 14:15:27 -0400

    two sum comments

--- twoSunSolution.js
+++ twoSunSolution.js
@@ -1,9 +1,9 @@
 function twoSum(numbers, target) {
   //given a target number get two numbers in the input array of numbers to equal the target
-  let finalArray = [];
-  for (let i = 0; i < numbers.length; i++) {
-    for (let j = i + 1; j < numbers.length; j++) {
-      if (numbers[i] + numbers[j] === target) {
+  let finalArray = [];      //set empty array to put ifinals into
+  for (let i = 0; i < numbers.length; i++) {        //loop thru numbers
+    for (let j = i + 1; j < numbers.length; j++) {      //loop thru again to compare j = i + 1 to compare the next index
+      if (numbers[i] + numbers[j] === target) {     //if the two numbers added up equal the target but those indexs in final array
         finalArray.push(i, j);
         return finalArray;
       }
