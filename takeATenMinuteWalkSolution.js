7a081ea07961d9edda21ba0b88bb2d925876963f
Ricky Martinez <RMartinezW16@gmail.com>
2022-10-05 14:44:09 -0400

    ten minute walk

--- takeATenMinuteWalkSolution.js
+++ takeATenMinuteWalkSolution.js
@@ -1,11 +1,11 @@
 function isValidWalk(walk) {
-  let ns = 0, we = 0;
-  for (let dir of walk) {
-    if (dir == "n") ns += 1;
-    if (dir == "s") ns -= 1;
-    if (dir == "w") we += 1;
-    if (dir == "e") we -= 1;
+  let ns = 0, we = 0;       //set two vars to count north south and west east
+  for (let dir of walk) {       //loop thru the walk array
+    if (dir == "n") ns += 1;    //if the direction in the array is n add 1 to the ns var
+    if (dir == "s") ns -= 1;    //if the direction in the array is s sub 1 to the ns var
+    if (dir == "w") we += 1;    //if the direction in the array is w add 1 to the we var
+    if (dir == "e") we -= 1;    //if the direction in the array is e sub 1 to the we var
   }
 
-  return walk.length == 10 && ns === 0 && we === 0;
+  return walk.length == 10 && ns === 0 && we === 0;     //return true if walk length is 10 and ns and we are 0. else false
 }
