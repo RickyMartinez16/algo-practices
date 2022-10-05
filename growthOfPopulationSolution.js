6ee37ecd61374d4026adac02b4ef0c854ee86e0d
Ricky Martinez <RMartinezW16@gmail.com>
2022-10-05 11:27:21 -0400

    gorwht population comments

--- growthOfPopulationSolution.js
+++ growthOfPopulationSolution.js
@@ -1,11 +1,11 @@
 function nbYear(p0, percent, aug, p, years = 0) {
-  return p0 >= p
-    ? years
+  return p0 >= p        //if p0 is equal to or greater than p return years(set to 0 in input)
+    ? years             //if not recursive function run it again
     : nbYear(
-        Math.floor(p0 + p0 * (percent / 100)) + aug,
-        percent,
-        aug,
-        p,
-        years + 1
+        Math.floor(p0 + p0 * (percent / 100)) + aug,        //round down for p0
+        percent,    //percent
+        aug,        //aug
+        p,          //p
+        years + 1    //years plus 1
       );
 }
