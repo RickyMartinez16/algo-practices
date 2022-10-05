1cd36d0566450b530ac8ab5a4148904eaad51c8c
Ricky Martinez <RMartinezW16@gmail.com>
2022-10-05 11:30:11 -0400

    best practice

--- growthOfPopulationSolution.js
+++ growthOfPopulationSolution.js
@@ -1,11 +1,22 @@
 function nbYear(p0, percent, aug, p, years = 0) {
-  return p0 >= p        //if p0 is equal to or greater than p return years(set to 0 in input)
-    ? years             //if not recursive function run it again
+  return p0 >= p //if p0 is equal to or greater than p return years(set to 0 in input)
+    ? years //if not recursive function run it again
     : nbYear(
-        Math.floor(p0 + p0 * (percent / 100)) + aug,        //round down for p0
-        percent,    //percent
-        aug,        //aug
-        p,          //p
-        years + 1    //years plus 1
+        Math.floor(p0 + p0 * (percent / 100)) + aug, //round down for p0
+        percent, //percent
+        aug, //aug
+        p, //p
+        years + 1 //years plus 1
       );
 }
+
+//easier to read//
+
+function nbYear(p0, percent, aug, p) {
+  for (var years = 0; p0 < p; years++) {        //loop thru while p0 is less than p
+    p0 = Math.floor(p0 + (p0 * percent) / 100 + aug);       //change the p0 and check again
+  }
+  return years;
+}
+
+//can also use while loop?? ^^^ 
