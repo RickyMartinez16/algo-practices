60210d0df381e51d050992d7745c0ea8908e5d1a
Ricky Martinez <RMartinezW16@gmail.com>
2022-10-06 15:47:32 -0400

    lowest and highets comments

--- lowestAndHighestSolution.js
+++ lowestAndHighestSolution.js
@@ -1,15 +1,15 @@
 function minMax(arr) {
-  let final1 = [];
-  if (arr.length === 1) {
-    final1.push(arr[0], arr[0]);
-    return final1;
+  let final1 = [];      //final answer array
+  if (arr.length === 1) {       //if theres only one number in the input
+    final1.push(arr[0], arr[0]);        //push it into final array twice
+    return final1;      //return final
   }
-  let final = [];
-  let sortedArr = arr.sort(function (a, b) {
+  let final = [];       //final var empty array
+  let sortedArr = arr.sort(function (a, b) {        //sort the array
     return a - b;
   });
 
-  final.push(sortedArr.shift());
-  final.push(sortedArr.pop());
-  return final;
+  final.push(sortedArr.shift());        //push the first number into it
+  final.push(sortedArr.pop());          //push the last number into it
+  return final;                         //reutn the final
 }
