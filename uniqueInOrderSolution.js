573a4d21ea8f5e3176692fe36f3185d9c3f5e6c1
Ricky Martinez <RMartinezW16@gmail.com>
2022-10-06 15:37:24 -0400

    unqie in order

---
+++ uniqueInOrderSolution.js
@@ -0,0 +1,12 @@
+function uniqueInOrder(iterable) {
+  //your code here - remember iterable can be a string or an array
+  const result = [];
+
+  for (let i = 0; i < iterable.length; i++) {
+    if (iterable[i] !== iterable[i + 1]) {
+      result.push(iterable[i]);
+    }
+  }
+
+  return result;
+}
