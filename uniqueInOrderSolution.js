c955d08464c4501be41d432a32259f1e7547fd81
Ricky Martinez <RMartinezW16@gmail.com>
2022-10-06 15:39:26 -0400

    best practice

--- uniqueInOrderSolution.js
+++ uniqueInOrderSolution.js
@@ -10,3 +10,19 @@ function uniqueInOrder(iterable) {
 
   return result;        //return final array    
 }
+
+
+//------//
+
+function uniqueInOrder(it) {
+    var result = []
+    var last
+    
+    for (var i = 0; i < it.length; i++) {
+      if (it[i] !== last) {
+        result.push(last = it[i])
+      }
+    }
+    
+    return result
+  }
