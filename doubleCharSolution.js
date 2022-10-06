7a0a552a1254c675e44134e3c602821a8ed0bc8a
Ricky Martinez <RMartinezW16@gmail.com>
2022-10-06 16:11:32 -0400

    double char best

--- doubleCharSolution.js
+++ doubleCharSolution.js
@@ -6,3 +6,9 @@ function doubleChar(str) {
   }
   return final.join("");        //join the array as a string
 }
+
+
+//---------//
+
+const doubleChar = (str) => str.split("").map(c => c + c).join("");
+//split the string into an array, map the array creating a new one, each character C map it to C + C. then join at the end to make stirng
