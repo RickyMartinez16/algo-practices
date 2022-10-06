96714bfac04b745911934fca07b9d370249f4efb
Ricky Martinez <RMartinezW16@gmail.com>
2022-10-06 15:38:25 -0400

    comments on unique

--- uniqueInOrderSolution.js
+++ uniqueInOrderSolution.js
@@ -1,12 +1,12 @@
 function uniqueInOrder(iterable) {
   //your code here - remember iterable can be a string or an array
-  const result = [];
+  const result = [];        //empty array for final answer
 
-  for (let i = 0; i < iterable.length; i++) {
-    if (iterable[i] !== iterable[i + 1]) {
-      result.push(iterable[i]);
+  for (let i = 0; i < iterable.length; i++) {   //loop thru input
+    if (iterable[i] !== iterable[i + 1]) {      //compare each input to the next one
+      result.push(iterable[i]);     //if theyre not the same then push the frist one into the final array
     }
   }
 
-  return result;
+  return result;        //return final array    
 }
