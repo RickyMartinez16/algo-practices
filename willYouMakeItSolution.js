68e487bfd5edd475c053a6c7aa6191787a4a9348
Ricky Martinez <RMartinezW16@gmail.com>
2022-10-07 12:18:55 -0400

    best and comments

--- willYouMakeItSolution.js
+++ willYouMakeItSolution.js
@@ -1,7 +1,14 @@
 const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
-  if (fuelLeft * mpg >= distanceToPump) {
-    return true;
-  } else {
+  if (fuelLeft * mpg >= distanceToPump) {       //if fuel times miles per gallon is more than or equal to distance
+    return true;        //return true
+  } else {      //if not return false
     return false;
   }
 };
+
+
+//-----best------//
+
+function zeroFuel(distanceToPump, mpg, fuelLeft) {
+    return (distanceToPump > mpg * fuelLeft) ? false : true;        //ternery
+  }
