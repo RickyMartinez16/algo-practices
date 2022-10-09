d030612dd4607d4bd15f9e1cb6743d163adc7c90
Ricky Martinez <RMartinezW16@gmail.com>
2022-10-09 17:07:42 -0400

    traffic light switch break

--- trafficLightSolution.js
+++ trafficLightSolution.js
@@ -1,11 +1,32 @@
 function updateLight(current) {
-  
-    if(current === "green"){
-      return "yellow"
-    }else if (current === "yellow"){
-      return "red"
-    } else if (current === "red"){
-      return "green"
+  if (current === "green") {
+    return "yellow";
+  } else if (current === "yellow") {
+    return "red";
+  } else if (current === "red") {
+    return "green";
+  }
+}
+
+
+//------swtich break------//
+
+
+function updateLight(current) {
+
+    switch (current) {
+      case 'green':
+        return 'yellow';
+        break;
+      case 'yellow':
+        return 'red';
+        break;
+      case 'red':
+        return 'green';
+        break;
+      default:
+        throw 'Error: wrong input';
+        break;
     }
-  
+    
   }
