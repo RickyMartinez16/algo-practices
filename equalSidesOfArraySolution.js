497925f33c9e481e37d66b1885d67a8b0bcda9cd
Ricky Martinez <RMartinezW16@gmail.com>
2022-10-10 11:34:50 -0400

    equal sides comments

--- equalSidesOfArraySolution.js
+++ equalSidesOfArraySolution.js
@@ -1,21 +1,21 @@
 function findEvenIndex(arr) {
-  let left = 0;
-  let right = 0;
-  let reducer = (accumulator, currentValue) => currentValue + accumulator;
+  let left = 0;     //set left side of array var
+  let right = 0;    //set right side of array var
+  let reducer = (accumulator, currentValue) => currentValue + accumulator;      //create reducer function
 
-  for (let i = 0; i < arr.length; i++) {
-    if (i == 0) {
-      right = arr.slice(1).reduce(reducer, 0);
-      if (i == right) {
-        return i;
+  for (let i = 0; i < arr.length; i++) {        //loop thru the array
+    if (i == 0) {       //if the index is 0
+      right = arr.slice(1).reduce(reducer, 0);      //set the right var to slice the first int in array and reduce all to the right
+      if (i == right) {     //if i eqauls right 
+        return i;       //return i
       }
-    } else {
-      left = arr.slice(0, i).reduce(reducer, 0);
-      right = arr.slice(i + 1).reduce(reducer, 0);
-      if (left == right) {
-        return i;
+    } else {  //if index is not zero
+      left = arr.slice(0, i).reduce(reducer, 0);        //set left side to slice 0 to i and then reduce them 
+      right = arr.slice(i + 1).reduce(reducer, 0);      //set right side to slice i + 1 and reduce them to the right
+      if (left == right) {      //if theyre the same 
+        return i;       //return i
       }
     }
   }
-  return -1;
+  return -1;        //if no answer return -1
 }
