21e2b1a483b21b61301eaf77565f2cc67163a05e
Ricky Martinez <RMartinezW16@gmail.com>
2022-10-10 13:07:11 -0400

    mumbling

--- mumblingSolution.js
+++ mumblingSolution.js
@@ -7,16 +7,16 @@
 // }
 
 function accum(s) {
-    const result = []
-    const lowerStr = s.toLowerCase()
+    const result = []       //set an empty array
+    const lowerStr = s.toLowerCase()        //lowercase the input
   
-    for(let i = 0; i < lowerStr.length; i++) {
-      let str = lowerStr[i].toUpperCase()
-      for(let j = 0; j < i; j++) {
-        str += lowerStr[i]
+    for(let i = 0; i < lowerStr.length; i++) {      // loop thru the lowercase input
+      let str = lowerStr[i].toUpperCase()       //set a new var str to uppercase the first letter
+      for(let j = 0; j < i; j++) {      //loop thru again
+        str += lowerStr[i]              //str var equals the lowercase string i
       }
-      result.push(str)
+      result.push(str)      //push the str into the array
     }
   
-    return result.join("-")
+    return result.join("-")     //join the array with a -
   }
