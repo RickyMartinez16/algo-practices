7afbe354e381d7bfa841e94edaad901ce552e21b
Ricky Martinez <RMartinezW16@gmail.com>
2022-10-11 10:35:16 -0400

    comments

--- twiceAsOldSolution.js
+++ twiceAsOldSolution.js
@@ -1,5 +1,5 @@
 function twiceAsOld(dadYearsOld, sonYearsOld) {
-  let twice = sonYearsOld * 2;
-  let difference = dadYearsOld - twice;
-  return Math.abs(difference);
+  let twice = sonYearsOld * 2;      //find twice as old of the summer aka target 
+  let difference = dadYearsOld - twice;     //find the difference in years
+  return Math.abs(difference);      //return absolute number of difference 
 }
