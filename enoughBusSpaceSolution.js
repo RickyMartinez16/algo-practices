99d19b819ef7ea5393e6541dbc13ebe336e89308
Ricky Martinez <RMartinezW16@gmail.com>
2022-10-12 10:46:42 -0400

    comments bus

--- enoughBusSpaceSolution.js
+++ enoughBusSpaceSolution.js
@@ -1,6 +1,6 @@
 function enough(cap, on, wait) {
     if(on + wait > cap){        //if on and wait is greater than cap
-      return Math.abs(cap - (wait + on))        //find the difference 
+      return Math.abs(cap - (wait + on))        //find the difference math abs makes number postive
     } else {
       return 0      //if all good return 0
     }
