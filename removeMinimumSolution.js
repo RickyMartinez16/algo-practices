011c50451428c13e70dcdd0036e4301506e1e0e9
Ricky Martinez <RMartinezW16@gmail.com>
2022-10-14 20:02:03 -0400

    not working comments

--- removeMinimumSolution.js
+++ removeMinimumSolution.js
@@ -3,10 +3,10 @@
 function removeSmallest(numbers) {
     let newArray =  new Array(...numbers) //tried putting the numers into a new array to not mess up the original array
     
-    let sortedNumbers = newArray.sort(function(a, b){return a-b});
-    let array = []
-    for(let i = 1; i < sortedNumbers.length; i++){
-      array.push(sortedNumbers[i])
+    let sortedNumbers = newArray.sort(function(a, b){return a-b}); //sort the new array
+    let array = []      //make new array
+    for(let i = 1; i < sortedNumbers.length; i++){      //loop thru sorted array skipping first number
+      array.push(sortedNumbers[i])      //pushing the numbers into new array
     }
     return array
   }
