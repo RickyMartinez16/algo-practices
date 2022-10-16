1bf8519291441d661e466a1317f79ccc60351935
Ricky Martinez <RMartinezW16@gmail.com>
2022-10-15 22:37:29 -0400

    keep the loop

--- keepTheLoopSolution.js
+++ keepTheLoopSolution.js
@@ -1,3 +1,3 @@
 function hoopCount (n) {
-    return n >= 10 ? "Great, now move on to tricks" : "Keep at it until you get it" 
+    return n >= 10 ? "Great, now move on to tricks" : "Keep at it until you get it" //ternert it ewual to orgreater than 10 return first option
  }
