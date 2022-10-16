4e9f527f1fd4326e722bdb0d0cac2258f92586f6
Ricky Martinez <RMartinezW16@gmail.com>
2022-10-16 12:46:11 -0400

    comments

--- fakeBinarySolution.js
+++ fakeBinarySolution.js
@@ -1,11 +1,11 @@
 function fakeBin(x) {
-  let string = "";
-  for (let i = 0; i < x.length; i++) {
-    if (x[i] < 5) {
-      string += "0";
+  let string = "";      //set an empty string
+  for (let i = 0; i < x.length; i++) {      //loop thru input string
+    if (x[i] < 5) {     //if the selected letter in the string is less than 5
+      string += "0";        //add 0 to the string
     } else {
-      string += "1";
+      string += "1";        //if its greater than 5 add 1 to the string
     }
   }
-  return string;
+  return string;        //return final string
 }
