5adf5406ad0c954cc6bb1611b79ed7c51094ed4c
Ricky Martinez <RMartinezW16@gmail.com>
2022-10-16 12:22:00 -0400

    grasshopper comments

--- grasshopperSolution.js
+++ grasshopperSolution.js
@@ -1,14 +1,14 @@
-function getGrade (s1, s2, s3) {
-    let sum = (s1 + s2 + s3) / 3
-    if(sum >= 90){
-      return "A"
-    } else if(sum >= 80 && sum < 90){
-      return "B"
-    } else if(sum >= 70 && sum < 80){
-      return "C"
-    } else if (sum >= 60 && sum < 70){
-      return "D"
-    } else{
-      return "F"
-    }
+function getGrade(s1, s2, s3) {
+  let sum = (s1 + s2 + s3) / 3;     //get the sum st to var
+  if (sum >= 90) {      //check for each condition
+    return "A";
+  } else if (sum >= 80 && sum < 90) {
+    return "B";
+  } else if (sum >= 70 && sum < 80) {
+    return "C";
+  } else if (sum >= 60 && sum < 70) {
+    return "D";
+  } else {
+    return "F";
   }
+}
