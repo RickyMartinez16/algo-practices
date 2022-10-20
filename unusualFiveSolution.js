27716cc5dbcb04c73e3acbe060514de5af12000a
Ricky Martinez <RMartinezW16@gmail.com>
2022-10-20 11:46:16 -0400

    unsuual five

---
+++ unusualFiveSolution.js
@@ -0,0 +1,9 @@
+function unusualFive() {
+  let array = ["a", "b", "c", "d", "e", "f"];   //make an array of 6 elements
+  return array.indexOf("f");        //find the index 5
+}
+
+
+function unusualFive() {
+    return 'fucku'.length
+  }
