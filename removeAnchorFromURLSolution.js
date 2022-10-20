86d66488e7c0446f752a6800b03b810617300a4a
Ricky Martinez <RMartinezW16@gmail.com>
2022-10-19 20:47:35 -0400

    remove anchor

--- removeAnchorFromURLSolution.js
+++ removeAnchorFromURLSolution.js
@@ -1,3 +1,11 @@
 function removeUrlAnchor(url){
     return url.split('#')[0]
    }
+
+
+
+
+   //----------------//
+
+
+  // https://bobbyhadz.com/blog/javascript-remove-everything-after-specific-character-in-string
