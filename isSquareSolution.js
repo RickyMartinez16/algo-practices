28e0477a8e932c4dd5df847d8891314b3f3e8870
Ricky Martinez <RMartinezW16@gmail.com>
2022-10-19 21:17:07 -0400

    square comments

--- isSquareSolution.js
+++ isSquareSolution.js
@@ -1,3 +1,3 @@
 function isSquare(n) {
-    return Math.sqrt(n) % 1 === 0;
+    return Math.sqrt(n) % 1 === 0;      //get the square root of n and if the remainder of divising by 1 is 0 is perfect
   }
