9d25240d137ad209165970f2a9088e303362f141
Ricky Martinez <RMartinezW16@gmail.com>
2022-10-19 21:44:21 -0400

    find middle el best

--- findMiddleElementSolution.js
+++ findMiddleElementSolution.js
@@ -1,3 +1,13 @@
 function gimme(a) {
-    return a.indexOf(a.concat().sort(function(a, b) { return a - b })[1])
-  }
+  return a.indexOf(
+    a.concat().sort(function (a, b) {
+      return a - b;
+    })[1]
+  );
+}
+
+const gimme = function (arr) {
+  return arr.indexOf([...arr].sort((x, y) => x > y)[1]);
+};
+
+
