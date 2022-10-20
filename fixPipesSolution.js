2dda05228db6ae21c6e18fd2ef4107e7f69075f6
Ricky Martinez <RMartinezW16@gmail.com>
2022-10-20 12:30:38 -0400

    fix pipes

---
+++ fixPipesSolution.js
@@ -0,0 +1,10 @@
+function pipeFix(numbers) {
+  var first = numbers[0];       //first at first int
+  var last = numbers[numbers.length - 1];       //find last int
+
+  var arr = [];     //make a new array
+  for (var i = first; i <= last; i++) {     //loop thru starting at first and going until i eqauls last
+    arr.push(i);        //push every int into new array
+  }
+  return arr;
+}
