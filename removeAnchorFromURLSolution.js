bb96071f64863c265ba57fb61a1ccb49a5bd1ad4
Ricky Martinez <RMartinezW16@gmail.com>
2022-10-19 20:49:20 -0400

    comments on anchor

--- removeAnchorFromURLSolution.js
+++ removeAnchorFromURLSolution.js
@@ -3,7 +3,9 @@ function removeUrlAnchor(url){
    }
 
 
+//    We used the String.split method to split on the bracket # character.
 
+//     The split() method returned an array of 2 substrings - the parts of the string before and after the character.
 
    //----------------//
 
