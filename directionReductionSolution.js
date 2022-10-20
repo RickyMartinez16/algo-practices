2485227d8ca4c75a2486e216df387dedcff675a5
Ricky Martinez <RMartinezW16@gmail.com>
2022-10-19 20:47:00 -0400

    remove anchor

--- directionReductionSolution.js
+++ directionReductionSolution.js
@@ -1,11 +1,11 @@
 function dirReduc(plan) {
-  var opposite = {
+  var opposite = {      //make an object with key values containing opposite of each key
     NORTH: "SOUTH",
     EAST: "WEST",
     SOUTH: "NORTH",
     WEST: "EAST",
   };
-  return plan.reduce(function (dirs, dir) {
+  return plan.reduce(function (dirs, dir) {             
     if (dirs[dirs.length - 1] === opposite[dir]) dirs.pop();
     else dirs.push(dir);
     return dirs;
