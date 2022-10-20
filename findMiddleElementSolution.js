b759de1ab703fc1ed1867c71ac0c4a273bf18379
Ricky Martinez <RMartinezW16@gmail.com>
2022-10-19 21:47:01 -0400

    find middle

--- findMiddleElementSolution.js
+++ findMiddleElementSolution.js
@@ -4,10 +4,13 @@ function gimme(a) {
       return a - b;
     })[1]
   );
-}
+} 
+
+//find trhe index of the sorted array index 1 in a
 
 const gimme = function (arr) {
   return arr.indexOf([...arr].sort((x, y) => x > y)[1]);
 };
 
-
+//spred operator
+//find trhe index of the sorted array index 1 in a
