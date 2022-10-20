732594d3610280ccc9dbc69c0921b71eec464a7a
Ricky Martinez <RMartinezW16@gmail.com>
2022-10-20 11:20:11 -0400

    fill the array

---
+++ fillAnArraySolution.js
@@ -0,0 +1,7 @@
+function arr(n) {
+  var newArr = [];      //set new array
+  for (var i = 0; i < n; i++) {     //loop as long as number is less than N
+    newArr.push(i);     //push the new number into the new array
+  }
+  return newArr;
+}
