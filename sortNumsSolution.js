08a55ed0656b2bfa64caac80dbd29c76c5e99baa
Ricky Martinez <RMartinezW16@gmail.com>
2022-10-19 21:30:19 -0400

    sort nums

--- sortNumsSolution.js
+++ sortNumsSolution.js
@@ -1,6 +1,6 @@
 function solution(nums){
-    if(!nums){
+    if(!nums){      //if nums is null return empty array
       return []
     }
-    return nums.sort(function(a, b){return a-b});
+    return nums.sort(function(a, b){return a-b});       //sort nums
   }
