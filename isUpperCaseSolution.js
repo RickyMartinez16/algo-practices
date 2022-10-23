ac163be93d154b15289f0ca5d998910e3e001288
Ricky Martinez <RMartinezW16@gmail.com>
2022-10-22 20:56:19 -0400

    is upperCase

--- isUpperCaseSolution.js
+++ isUpperCaseSolution.js
@@ -1,3 +1,8 @@
 String.prototype.isUpperCase = function () {
     return !/[a-z]/.test(this);
   };
+
+
+  String.prototype.isUpperCase = function() {
+    return this.toUpperCase() === this.toString();
+  }
