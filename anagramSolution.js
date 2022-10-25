48a7b8f8856e43c1bfa8c51b6ecc6b2c4e9a5dd1
Ricky Martinez <RMartinezW16@gmail.com>
2022-10-25 19:31:53 -0400

    anamgram comments

--- anagramSolution.js
+++ anagramSolution.js
@@ -1,8 +1,8 @@
 var isAnagram = function (test, original) {
-  let newString = test.toLowerCase().split("").sort().join("").toLowerCase();
-  let otherNewString = original.toLowerCase().split("").sort().join("");
+  let newString = test.toLowerCase().split("").sort().join("").toLowerCase();       //sort the test string
+  let otherNewString = original.toLowerCase().split("").sort().join("");        //sort the og string
 
-  if (newString === otherNewString) {
+  if (newString === otherNewString) {       //compare the strings
     return true;
   } else {
     return false;
