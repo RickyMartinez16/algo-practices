8b894179e770cf67704dc2dfea4c88779000376b
Ricky Martinez <RMartinezW16@gmail.com>
2022-10-24 20:43:37 -0400

    dna ro rna

--- DNAtoRNASolution.js
+++ DNAtoRNASolution.js
@@ -1,4 +1,4 @@
 function DNAtoRNA(dna) {
-  let regex = /([T])/g;
-  return dna.replace(regex, "U");
+  let regex = /([T])/g;     //make a regex to find all the captail Ts
+  return dna.replace(regex, "U");    //replace all the regex with the Us
 }
