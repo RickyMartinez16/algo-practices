db6c96b0ce9ad1a8d6149ccc57934bdf113284da
Ricky Martinez <RMartinezW16@gmail.com>
2022-10-24 21:26:37 -0400

    remove exclmaton

--- removeLastExclamationSolution.js
+++ removeLastExclamationSolution.js
@@ -1,6 +1,6 @@
 function remove (string) {
-    if(string.charAt(string.length -1) === "!"){
-      return string.slice(0, -1)
+    if(string.charAt(string.length -1) === "!"){        //if the last character of the string is an exlmcation point
+      return string.slice(0, -1)        //return the string slicing off the last letter
     }
-    return string
+    return string       //if not return the ogirinal stirng
   }
