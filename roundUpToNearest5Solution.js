c6915354d5af25375020303c44bd746e204f963d
Ricky Martinez <RMartinezW16@gmail.com>
2022-10-24 20:57:41 -0400

    round to nearest 5

--- roundUpToNearest5Solution.js
+++ roundUpToNearest5Solution.js
@@ -1,6 +1,13 @@
 function roundToNext5(n) {
-  while (n % 5 !== 0) {     //while the remainder of n divideed by 5 does not eqaul 0
-    n++;                    //add one to n 
+  while (n % 5 !== 0) {
+    //while the remainder of n divideed by 5 does not eqaul 0
+    n++; //add one to n
   }
-  return n;     //return the final n
+  return n; //return the final n
+}
+
+//-------best------//
+
+function roundToNext5(n) {
+  return Math.ceil(n / 5) * 5;
 }
