603485ba039b8fb2f2634ac5d0bc0f6d88022ee9
Ricky Martinez <RMartinezW16@gmail.com>
2022-10-25 19:54:09 -0400

    calyclaye average

--- calculateAverageSolution.js
+++ calculateAverageSolution.js
@@ -1,11 +1,11 @@
 function findAverage(array) {
   if (array.length === 0) {
-    return 0;
+    return 0;               //if the array is empty return 0
   }
   const initialValue = 0;
   const sum = array.reduce(
     (previousValue, currentValue) => previousValue + currentValue,
     initialValue
-  );
-  return sum / array.length;
+  );                                //redeuce the array to one number
+  return sum / array.length;        //divide the reduced array by the length of the array
 }
