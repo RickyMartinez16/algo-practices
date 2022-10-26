8326828e7fab9373a4c163496ef9243ea882a816
Ricky Martinez <RMartinezW16@gmail.com>
2022-10-26 18:20:39 -0400

    factorial comments

--- factorialSolution.js
+++ factorialSolution.js
@@ -1,6 +1,7 @@
 function factorial(n) {
-  if (n < 0 || n > 12) throw new RangeError();
-  return n <= 1 ? 1 : n * factorial(n - 1);
+  if (n < 0 || n > 12) throw new RangeError();  //if n is less than 0 or greater than 12 throw new range error
+  return n <= 1 ? 1 : n * factorial(n - 1);     //if n is less than or equal to 1 return 1 
+                                                //if not n times recursive function factorial but this time n - 1
 }
 
 function factorial(n) {
