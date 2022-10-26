19db860b0c4e42f2d6f5815351c55eebc6f61161
Ricky Martinez <RMartinezW16@gmail.com>
2022-10-26 18:03:53 -0400

    survive dragons

---
+++ surviveDragonsSolution.js
@@ -0,0 +1,11 @@
+function hero(bullets, dragons){
+    return bullets / 2 >= dragons ? true : false
+  }
+
+  
+  function hero(bullets, dragons){
+  if(bullets / 2 >= dragons){
+    return true
+  } 
+  return false
+}
