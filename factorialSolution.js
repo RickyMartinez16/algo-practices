257f847bbf21d1c9a343fde8df254994ac7dbf5f
Ricky Martinez <RMartinezW16@gmail.com>
2022-10-26 18:18:37 -0400

    factorial

--- factorialSolution.js
+++ factorialSolution.js
@@ -1,5 +1,11 @@
 function factorial(n) {
-    if (n < 0 || n > 12)
-      throw new RangeError();
-    return n <= 1 ? 1 : n * factorial(n - 1);
-  }
+  if (n < 0 || n > 12) throw new RangeError();
+  return n <= 1 ? 1 : n * factorial(n - 1);
+}
+
+function factorial(n) {
+  if (n < 0 || n > 12) throw RangeError();
+  var f = 1;
+  while (n > 1) f *= n--;
+  return f;
+}
