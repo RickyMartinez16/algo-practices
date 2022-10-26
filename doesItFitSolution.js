836d93baa6f46255d575ac0e3e45f177f6875fc0
Ricky Martinez <RMartinezW16@gmail.com>
2022-10-26 17:52:35 -0400

    does it fit comment

--- doesItFitSolution.js
+++ doesItFitSolution.js
@@ -1,8 +1,8 @@
 function smallEnough(a, limit) {
-  for (let i = 0; i < a.length; i++) {
-    if (a[i] > limit) {
+  for (let i = 0; i < a.length; i++) {      //loop thru array
+    if (a[i] > limit) {     //if a[i] is greater than limit return false
       return false;
     }
   }
-  return true;
+  return true;      //if loop completes return true
 }
