5d838af07200fb09da6e4c40af61f7a150d31c4f
Ricky Martinez <RMartinezW16@gmail.com>
2022-10-29 16:46:58 -0400

    super easy

--- superEasySolution.js
+++ superEasySolution.js
@@ -1,3 +1,3 @@
 function problem(x){
-    return typeof x === "string" ? "Error" : x * 50 + 6
+    return typeof x === "string" ? "Error" : x * 50 + 6  //ternery if type of x is string return error or do the math
   }
