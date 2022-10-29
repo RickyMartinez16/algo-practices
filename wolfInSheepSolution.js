6a36d30ca2ff5a062ef09293475fdca19c5c5b2c
Ricky Martinez <RMartinezW16@gmail.com>
2022-10-29 16:31:18 -0400

    wolrdin sheeps comment

--- wolfInSheepSolution.js
+++ wolfInSheepSolution.js
@@ -1,10 +1,8 @@
 function warnTheSheep(queue) {
-    console.log(queue.indexOf("wolf"))
-    console.log(queue.length)
-    if(queue[queue.length - 1] === "wolf"){
-      return "Pls go away and stop eating my sheep"
-    } else {
-      let index = queue.indexOf("wolf")
-      return `Oi! Sheep number ${(queue.length - 1) - index}! You are about to be eaten by a wolf!`
-    }
+  if (queue[queue.length - 1] === "wolf") {     //if the queue at the last index equals wolf
+    return "Pls go away and stop eating my sheep";      //return message
+  } else {
+    let index = queue.indexOf("wolf");  //find the index of wolf
+    return `Oi! Sheep number ${queue.length - 1 - index}! You are about to be eaten by a wolf!`;  //return message with mathed index postion
   }
+}
