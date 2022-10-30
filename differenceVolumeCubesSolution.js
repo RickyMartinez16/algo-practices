7442d6fd953ed479cd132bb98d55f6684ffd97a4
Ricky Martinez <RMartinezW16@gmail.com>
2022-10-30 12:37:38 -0400

    volume cubes comments

--- differenceVolumeCubesSolution.js
+++ differenceVolumeCubesSolution.js
@@ -1,13 +1,13 @@
 function findDifference(a, b) {
-  const sumA = a.reduce(
+  const sumA = a.reduce(        //reduce the first array
     (previousValue, currentValue) => previousValue * currentValue,
     1
   );
   console.log(sumA);
-  const sumB = b.reduce(
+  const sumB = b.reduce(        //reduce the second array
     (previousValue, currentValue) => previousValue * currentValue,
     1
   );
   console.log(sumB);
-  return Math.abs(sumA - sumB);
+  return Math.abs(sumA - sumB);     //absolute value so its alwasy positive diffrence of the two arrays
 }
