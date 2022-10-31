54483a9727b6905ea68cbbb25a11bcd2b7b0b0a0
Ricky Martinez <RMartinezW16@gmail.com>
2022-10-31 11:38:26 -0400

    no boring zeros comments

--- noBoringZerosSolution.js
+++ noBoringZerosSolution.js
@@ -1,9 +1,9 @@
 function noBoringZeros(n) {
-  let nArray = String(n).split("");
-  console.log(nArray);
-  while (nArray[nArray.length - 1] === "0") {
-    nArray = nArray.slice(0, nArray.length - 1);
+  let nArray = String(n).split(""); //turn the input to a string and then split it into an array
+  
+  while (nArray[nArray.length - 1] === "0") {   //while the end of the array is 0
+    nArray = nArray.slice(0, nArray.length - 1);    //slice off the end of the array
   }
-  console.log(nArray);
-  return Number(nArray.join(""));
+  
+  return Number(nArray.join(""));   //join the remaining array to a single string and then turn string into a number
 }
