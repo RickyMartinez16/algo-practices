2b2977660f0de4c83d1ce6d294afe5fa62c43395
Ricky Martinez <RMartinezW16@gmail.com>
2022-10-31 11:56:15 -0400

    multiplcation table:

--- multiplicationTableSolution.js
+++ multiplicationTableSolution.js
@@ -1,9 +1,9 @@
 const multiTable = (number) => {
-    let table = '';
+    let table = '';     //set empty string var
     
-    for(let i = 1; i <= 10; i++) {
-      table += `${i} * ${number} = ${i * number}${i < 10 ? '\n' : ''}`; 
+    for(let i = 1; i <= 10; i++) {      //loop thru starting at 1 and ending at 10
+      table += `${i} * ${number} = ${i * number}${i < 10 ? '\n' : ''}`; //add this string to table var
     }
-  
-    return table;
+                                    //if i is less than 10 \n to make to line if not end
+    return table;   //return table
   }
