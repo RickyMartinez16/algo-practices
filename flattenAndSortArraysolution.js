25c2ee9a3feeceefc8e0e826a85cebf9da1721ea
Ricky Martinez <RMartinezW16@gmail.com>
2022-11-02 10:35:46 -0400

    flatten array comments

--- flattenAndSortArraysolution.js
+++ flattenAndSortArraysolution.js
@@ -1,8 +1,8 @@
 "use strict";
 
 function flattenAndSort(array) {
-  let finalArray = array.flat();
-  return finalArray.sort(function (a, b) {
+  let finalArray = array.flat();        //flatten the array of arrays into just one array
+  return finalArray.sort(function (a, b) {      //sort the finalarray and return
     return a - b;
   });
 }
