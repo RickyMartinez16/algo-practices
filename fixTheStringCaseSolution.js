80c1d908b0951affd9bf043d60e185f1439890d8
Ricky Martinez <RMartinezW16@gmail.com>
2022-11-02 11:12:43 -0400

    working solution for fix string case

--- fixTheStringCaseSolution.js
+++ fixTheStringCaseSolution.js
@@ -3,8 +3,10 @@
 function solve(s) {
   let uppercase = s.match(/^[A-Z]/g);
   console.log(uppercase);
+
   let lowercase = s.match(/^[a-a]/g);
   console.log(lowercase);
+
   if (uppercase && lowercase) {
     if (uppercase.length > lowercase.length) {
       var modified = s.replace(/^[A-Z]/g, function (match) {
@@ -12,6 +14,7 @@ function solve(s) {
       });
     }
   }
+
   if (uppercase && lowercase) {
     if (uppercase.length < lowercase.length) {
       var modified = s.replace(/^[a-z]/g, function (match) {
@@ -19,7 +22,25 @@ function solve(s) {
       });
     }
   }
+
   if (uppercase.length === lowercase.length) {
     return s.toLowerCase();
   }
+}
+
+
+//------------working-----------//
+
+function solve(s){
+    let lowerC = 0;
+    let upperC = 0;
+    for( let i = 0;i<s.length;i++){
+      if( s[i] == s[i].toUpperCase()){
+        upperC++;
+      }
+      else{
+        lowerC++;
+      }
+    }
+    return lowerC >= upperC ? s.toLowerCase() : s.toUpperCase()
 }
