aaa2b5addefc439b4cde7546612916a9bd8d29c6
Ricky Martinez <RMartinezW16@gmail.com>
2022-11-01 20:41:04 -0400

    sum of numbners

--- debugSumOfNumberSolution.js
+++ debugSumOfNumberSolution.js
@@ -1,11 +1,12 @@
 function getSumOfDigits(integer) {
-    let numberArray = String(integer).split("")
+    let numberArray = String(integer).split("")     //make the ints a string. split the string into an array
     
-    let map = numberArray.map(int => parseInt(int))
+    let map = numberArray.map(int => parseInt(int))     //map over the array, and then parse int to turn them into numbers
     
     const sum = map.reduce(
-      (previousValue, currentValue) => previousValue + currentValue,
+      (previousValue, currentValue) => previousValue + currentValue,    //reduce the map of numbers to one sum
       0
     );
-    return sum
+    return sum      //return sum
   }
+
