6a7abd2434fa71c6e1afdfea186f71601f274cbe
Ricky Martinez <RMartinezW16@gmail.com>
2022-11-03 13:02:12 -0400

    min max list

--- minMaxListSolution.js
+++ minMaxListSolution.js
@@ -1,9 +1,9 @@
 var min = function(list){
     
-    return list.sort(function(a, b){return a-b}).shift()
+    return list.sort(function(a, b){return a-b}).shift()        //sort the array then get the first
 }
 
 var max = function(list){
     
-    return list.sort(function(a, b){return a-b}).pop()
+    return list.sort(function(a, b){return a-b}).pop()          //sort the array then get the last
 }
