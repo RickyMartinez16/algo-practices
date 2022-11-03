6b8fd5ab5a0f651462157767e7ec19b3d6f16978
Ricky Martinez <RMartinezW16@gmail.com>
2022-11-03 13:11:15 -0400

    sum the strings format

--- sumTheStringsSolution.js
+++ sumTheStringsSolution.js
@@ -1,13 +1,13 @@
-function sumStr(a,b) {
-    if(!a && b){
-      a = 0
-      return String(parseInt(a) + parseInt(b))
-    } else if (!b && a){
-      b = 0
-      return String(parseInt(a) + parseInt(b))
-    } else if(!a && !b){
-      return '0'
-    } else {
-      return String(parseInt(a) + parseInt(b))
-    }
+function sumStr(a, b) {
+  if (!a && b) {
+    a = 0;
+    return String(parseInt(a) + parseInt(b));
+  } else if (!b && a) {
+    b = 0;
+    return String(parseInt(a) + parseInt(b));
+  } else if (!a && !b) {
+    return "0";
+  } else {
+    return String(parseInt(a) + parseInt(b));
   }
+}
