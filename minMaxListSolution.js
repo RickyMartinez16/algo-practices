8e240a86a498f4a995368d60c035719a80fe65c0
Ricky Martinez <RMartinezW16@gmail.com>
2022-11-03 13:02:54 -0400

    best

--- minMaxListSolution.js
+++ minMaxListSolution.js
@@ -7,3 +7,8 @@ var max = function(list){
     
     return list.sort(function(a, b){return a-b}).pop()          //sort the array then get the last
 }
+
+//-----best-----//
+
+const min = (list) => Math.min(...list);
+const max = (list) => Math.max(...list);
