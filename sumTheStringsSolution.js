ca6dd930b1699f3044f33983543e8f956f068523
Ricky Martinez <RMartinezW16@gmail.com>
2022-11-06 14:28:11 -0500

    sum the string

--- sumTheStringsSolution.js
+++ sumTheStringsSolution.js
@@ -2,9 +2,9 @@ function sumStr(a, b) {
   if (!a && b) {            //if no a but b is truthy
     a = 0;      //make a to 0 
     return String(parseInt(a) + parseInt(b));       //add them and then stringy them
-  } else if (!b && a) {
+  } else if (!b && a) {     //if b is falsy and a is truhy b is 0
     b = 0;
-    return String(parseInt(a) + parseInt(b));
+    return String(parseInt(a) + parseInt(b));       //add the numbers and return them as string
   } else if (!a && !b) {
     return "0";
   } else {
@@ -16,5 +16,5 @@ function sumStr(a, b) {
 //------best-----//
 
 function sumStr(a,b) {
-    return String(Number(a)+Number(b));
+    return String(Number(a)+Number(b));     //turn a and b into a number and add them and then return them as string
   }
