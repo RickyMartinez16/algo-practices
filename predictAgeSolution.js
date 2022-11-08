300a373077b11c5b6b8fea56cb9971f80e0167b0
Ricky Martinez <RMartinezW16@gmail.com>
2022-11-08 12:48:37 -0500

    predict age best

--- predictAgeSolution.js
+++ predictAgeSolution.js
@@ -14,3 +14,11 @@ function predictAge(age1,age2,age3,age4,age5,age6,age7,age8){
     
     return Math.floor(square / 2)
   }
+
+
+//-----------//
+
+function predictAge(age1,age2,age3,age4,age5,age6,age7,age8){
+    let arr = [age1, age2, age3, age4, age5, age6, age7, age8]
+    return Math.floor(Math.sqrt(arr.map(a => a * a).reduce((b,c) => b + c)) / 2)
+  }
