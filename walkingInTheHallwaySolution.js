f4cbd94c9c6110f7f68f24182099c04d736d2e9c
Ricky Martinez <RMartinezW16@gmail.com>
2022-11-07 19:44:44 -0500

    best coments

--- walkingInTheHallwaySolution.js
+++ walkingInTheHallwaySolution.js
@@ -1,4 +1,4 @@
 function contact(hallway) {
-    const gaps = hallway.match(/>-*</g)
-    return !gaps ? -1 : Math.min(...gaps.map(s=>s.length)) >> 1
+    const gaps = hallway.match(/>-*</g)     //match the gaps
+    return !gaps ? -1 : Math.min(...gaps.map(s=>s.length)) >> 1     //if no matches return -1 else return min of spread gaps.map array
   }
