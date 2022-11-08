424a4b748ad57684b92dc1f1e8a499ce5df69a1c
Ricky Martinez <RMartinezW16@gmail.com>
2022-11-08 11:40:38 -0500

    sum digits

--- sumADigitNumberSolution.js
+++ sumADigitNumberSolution.js
@@ -1,11 +1,11 @@
 function sumDigits(number) {
-  let string = String(number);
+  let string = String(number);  //turn the input number into a string
 
-  let array = string.split("");
+  let array = string.split(""); //split the string into an array
 
-  let count = 0;
-  for (let i = 0; i < array.length; i++) {
-    if (array[i].match(/^[0-9]+$/)) count += Number(array[i]);
+  let count = 0;        //make a count var
+  for (let i = 0; i < array.length; i++) {      //loop thru the string array
+    if (array[i].match(/^[0-9]+$/)) count += Number(array[i]);  //if ezch element in the array is a number add to the count
   }
-  return count;
+  return count; //return the count
 }
