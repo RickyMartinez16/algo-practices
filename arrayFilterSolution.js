5510a317ff1119c8f3902f4cf2e3e01531ab6a88
Ricky Martinez <RMartinezW16@gmail.com>
2022-11-09 10:30:03 -0500

    array filter

--- arrayFilterSolution.js
+++ arrayFilterSolution.js
@@ -1,3 +1,5 @@
 function getEvenNumbers(numbersArray){
     return numbersArray.filter((int => int % 2 ===0))
   }
+
+  const getEvenNumbers = numbersArray => numbersArray.filter(num => num % 2 == 0);
