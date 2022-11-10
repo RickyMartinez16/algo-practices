1fd9e538e4c61342084e7aa074c258459e174da4
Ricky Martinez <RMartinezW16@gmail.com>
2022-11-10 18:42:47 -0500

    sum of numbers from n comms

--- sumOfNumbersFromNsolution.js
+++ sumOfNumbersFromNsolution.js
@@ -25,20 +25,20 @@ var SequenceSum = (function() {
     function SequenceSum() {}
   
     SequenceSum.showSequence = function(count) {
-      var sum = 0;
-      var str = "";
-      if (count === 0) {
-        return "0=0";
-      } else if (count < 0) {
-        return count + "<0";
+      var sum = 0;      //set a sum var
+      var str = "";     //set an empty string var
+      if (count === 0) {        //if input count = 0
+        return "0=0";       //return 
+      } else if (count < 0) {       //else if count is less than 0
+        return count + "<0";        //return
       } else {
-        for (var i = 0; i < count; i++) {
-          sum += i;
-          str += i + "+";
+        for (var i = 0; i < count; i++) {       //loop thru count
+          sum += i;     //sum equals plus i
+          str += i + "+";       //add 'i+' to the string
         };
-      sum += count;
-      str += count + " = " + sum;
-      return str;
+      sum += count;     //sum eqauls plus count
+      str += count + " = " + sum; //string += count = sum
+      return str; //return string
       };
     };
   
