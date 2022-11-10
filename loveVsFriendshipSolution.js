4ef053538a1ef48dd59e669cad8410b0afb6688d
Ricky Martinez <RMartinezW16@gmail.com>
2022-11-10 16:52:44 -0500

    love vs friendship comments

--- loveVsFriendshipSolution.js
+++ loveVsFriendshipSolution.js
@@ -1,7 +1,7 @@
 function wordsToMarks(str)
 {
-  var sum = 0;
-  for (let i = 0; i < str.length; i++)
-    sum += str.charCodeAt(i) - 96;
-  return sum;
+  var sum = 0;      //make a sum var
+  for (let i = 0; i < str.length; i++)      //loop thru string
+    sum += str.charCodeAt(i) - 96;      //sum equals plus charcode - 96
+  return sum;       //return sum 
 }
