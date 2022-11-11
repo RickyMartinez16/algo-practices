1698269342859ff3f3aac0d8c412bc811390e5ff
Ricky Martinez <RMartinezW16@gmail.com>
2022-11-11 11:18:39 -0500

    form the min comments

--- formTheMinSolution.js
+++ formTheMinSolution.js
@@ -1,8 +1,8 @@
 function minValue(values) {
-  let set = new Set();
-  values.forEach((number) => set.add(number));
-  let sorted = [...set].sort(function (a, b) {
+  let set = new Set();  //make a new set
+  values.forEach((number) => set.add(number));  //for each value in the input array add it to the set. the set will remove dups
+  let sorted = [...set].sort(function (a, b) {  //sort the new array
     return a - b;
   });
-  return Number(sorted.join(""));
+  return Number(sorted.join(""));   //join the array and make it a number
 }
