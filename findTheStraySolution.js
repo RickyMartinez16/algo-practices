e1bf4b6709c01a8ef4fb1d2781fb6a74d69788ca
Ricky Martinez <RMartinezW16@gmail.com>
2022-11-10 19:03:52 -0500

    find the stray

--- findTheStraySolution.js
+++ findTheStraySolution.js
@@ -1,10 +1,10 @@
 function stray(numbers) {
-  let sorted = numbers.sort(function (a, b) {
+  let sorted = numbers.sort(function (a, b) {       //sort the input array
     return a - b;
   });
-  if (sorted[0] === sorted[1]) {
-    return sorted[sorted.length - 1];
+  if (sorted[0] === sorted[1]) {        //if the first two numbers are the same 
+    return sorted[sorted.length - 1];   //return the last number
   } else {
-    return sorted[0];
+    return sorted[0];       //if not return the first number
   }
 }
