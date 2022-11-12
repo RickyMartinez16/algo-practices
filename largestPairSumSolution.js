17867391218c4190911c32246453cdf28c038db1
Ricky Martinez <RMartinezW16@gmail.com>
2022-11-12 13:55:48 -0500

    largest pair sum comments

--- largestPairSumSolution.js
+++ largestPairSumSolution.js
@@ -1,4 +1,4 @@
 function largestPairSum (numbers) {
-    let sorted = numbers.sort((a,b) => a-b)
-    return sorted[sorted.length - 1] + sorted[sorted.length - 2]
+    let sorted = numbers.sort((a,b) => a-b)     //sort input array
+    return sorted[sorted.length - 1] + sorted[sorted.length - 2]    //sum the two at the end of array
   }
