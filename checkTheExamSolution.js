5a052d199daf20fc0209e7a3b157ed09017c06e8
Ricky Martinez <RMartinezW16@gmail.com>
2022-11-14 10:38:20 -0500

    check exam

--- checkTheExamSolution.js
+++ checkTheExamSolution.js
@@ -1,13 +1,13 @@
 function checkExam(array1, array2) {
-    let count = 0;
-     for(let i = 0; i < array1.length; i++){
-       if(array2[i] === ""){
-         count += 0
-       } else if(array1[i] === array2[i]){
+    let count = 0;  //make count var
+     for(let i = 0; i < array1.length; i++){    //loop thru array1
+       if(array2[i] === ""){    //if array2 = ""
+         count += 0     //dont change count
+       } else if(array1[i] === array2[i]){  //if two sports are the same add 4 to count
          count +=4
        } else {
-         count -=1
+         count -=1  //if theyre not the same minus one from count
        }
      }
-     return count >= 0 ? count : 0
+     return count >= 0 ? count : 0  //if count is above or eqaul to zero return the count if not return 0
    }
