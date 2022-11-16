f8f554825220b2cb8ab2c1fcfbdf5d2c234b381d
Ricky Martinez <RMartinezW16@gmail.com>
2022-11-15 19:25:24 -0500

    find the position

--- findThePositionSolution.js
+++ findThePositionSolution.js
@@ -1,3 +1,3 @@
 function position(letter){
-    return `Position of alphabet: ${(letter.charCodeAt() - 96)}`
+    return `Position of alphabet: ${(letter.charCodeAt() - 96)}`    //return letter char code at subtracted 96 for correct position
   }
