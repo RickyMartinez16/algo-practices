b0409e237d9cc2030f6299cd188da443b2529162
Ricky Martinez <RMartinezW16@gmail.com>
2022-11-19 20:33:42 -0500

    sum f multiple final

--- sumOfMultiplesSolution.js
+++ sumOfMultiplesSolution.js
@@ -1,9 +1,9 @@
 function sumMul(n,m){
-    if (n >= m) return "INVALID";
+    if (n >= m) return "INVALID";       //if n is greater than or qual to n return invaled
   
-  var sum = 0;
-    for (var i = n; i < m; i+=n) {
-      sum += i;
+  var sum = 0;      //make a var to count the sum
+    for (var i = n; i < m; i+=n) {      // create a loop where i = n, i runs less than m, and i equals plus n
+      sum += i;     //sum equals plus i
     }
-    return sum;
+    return sum;     //retunr the sum
   }
