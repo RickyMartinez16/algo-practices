ec6415ca555abdd06ff23e4d8a3328df7af2a29d
Ricky Martinez <RMartinezW16@gmail.com>
2022-11-19 20:06:06 -0500

    highest scoring word comments "

--- highestScoringWordSolution.js
+++ highestScoringWordSolution.js
@@ -18,24 +18,24 @@ function high(x){
     let alphabets = [
       'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
       'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
-    ];
+    ];  // make alphabet array
     
-    let words = x.split(" ");
-    let highestScore = 0;
-    let highestScoreWord = "";
+    let words = x.split(" ");       //split the input sentence to indciual words
+    let highestScore = 0;       //make a var to keep the hghest score
+    let highestScoreWord = "";  //make a var to keep the highest word
     
-    for (let word of words) {
-      let lettersSum = 0;
+    for (let word of words) {       //loop thru the words at each indivuaual word
+      let lettersSum = 0;      //make a var to keep track of the sum
       
-      for (let letter of word) {
-        lettersSum += alphabets.indexOf(letter) + 1;
+      for (let letter of word) {    //loop thru each letter of each word one word at a time
+        lettersSum += alphabets.indexOf(letter) + 1;    //letter sum += alphabtets index plus one for proper number
       }
       
-      if (lettersSum > highestScore) {
-        highestScore = lettersSum;
-        highestScoreWord = word;
+      if (lettersSum > highestScore) {      //if the lettersum is greaer than highest score
+        highestScore = lettersSum;      //make the highest score equal to the nwe letter some
+        highestScoreWord = word;        //and the highest score word is the word you are looking at
       }
     }
     
-    return highestScoreWord;
+    return highestScoreWord;        //return the highest score word var
   }
