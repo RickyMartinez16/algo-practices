54bd84ee9c8ea9e9bb669fa7a30b81dc8f4c1d95
Ricky Martinez <RMartinezW16@gmail.com>
2022-11-19 20:01:46 -0500

    comments hiegst scoring

--- highestScoringWordSolution.js
+++ highestScoringWordSolution.js
@@ -1,12 +1,12 @@
 //not working//
 
 function high(x){
-    let arrayWords = x.split(" ")
+    let arrayWords = x.split(" ")   //split the strings to indivisual words
     let charCode = 0
     console.log(arrayWords)
-    for(let i = 0; i < arrayWords.length; i++){
-      let letters = arrayWords[i].split("")
-      for(let j = 0; j < letters[i].length; j++){
+    for(let i = 0; i < arrayWords.length; i++){     //loop thru the words
+      let letters = arrayWords[i].split("")     //split the words into letters
+      for(let j = 0; j < letters[i].length; j++){   ////loop thru the letters of each word
         charCode += charCodeAt(letters[i][j])
       }
     }
