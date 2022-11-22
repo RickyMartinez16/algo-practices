8f2806c4fb4a75f86273a0baef1fb795ed59963a
Ricky Martinez <RMartinezW16@gmail.com>
2022-11-21 19:49:57 -0500

    pre fixx buzz solved

--- preFizzBuzzSolution.js
+++ preFizzBuzzSolution.js
@@ -1,9 +1,9 @@
 function preFizz(n) {
-    let finalArray = [];
-    let newInt = 1
-    while(n >= newInt){
-      finalArray.push(newInt)
-      newInt++
+    let finalArray = [];  //nset new array var empty
+    let newInt = 1      //make a int to push
+    while(n >= newInt){     //while n is less than or equal to new int
+      finalArray.push(newInt)       //push the new int into the array
+      newInt++      //add one to the array
     }
-    return finalArray
+    return finalArray       //return final array
   }
