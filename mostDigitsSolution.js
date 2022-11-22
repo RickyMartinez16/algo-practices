1a69c98bfa48289d1da1d3aa0095567eb51d2b5d
Ricky Martinez <RMartinezW16@gmail.com>
2022-11-21 20:21:49 -0500

    most digits

--- mostDigitsSolution.js
+++ mostDigitsSolution.js
@@ -11,3 +11,17 @@ function findLongest(array){
     }
     return longNum
   }
+
+
+  //----working-----//
+
+  function findLongest(array){
+    let num = '0';
+    for(let i=0; i<array.length; i++) {
+      let string = array[i].toString();
+      if(string.length > num.length) {
+        num = string;
+      }
+    }
+    return parseInt(num);
+  }
