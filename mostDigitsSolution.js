eb39a13ab53dee0f6fb96e887609a8ab6582bcad
Ricky Martinez <RMartinezW16@gmail.com>
2022-11-21 20:23:21 -0500

    most digits

--- mostDigitsSolution.js
+++ mostDigitsSolution.js
@@ -16,12 +16,12 @@ function findLongest(array){
   //----working-----//
 
   function findLongest(array){
-    let num = '0';
-    for(let i=0; i<array.length; i++) {
-      let string = array[i].toString();
-      if(string.length > num.length) {
-        num = string;
+    let num = '0';      //make num a string var
+    for(let i=0; i<array.length; i++) { //loop thru input array
+      let string = array[i].toString();     //make a string var which is each number in the input array converted to string
+      if(string.length > num.length) {  //if string .length is larger than num.length
+        num = string;   //then num is now the string
       }
     }
-    return parseInt(num);
+    return parseInt(num);       //parse the string to return a num
   }
