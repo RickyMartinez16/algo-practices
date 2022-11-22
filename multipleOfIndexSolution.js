4f26f04580d9585a984fbdc1bfa83632454c8995
Ricky Martinez <RMartinezW16@gmail.com>
2022-11-22 08:52:22 -0500

    multiple of index

--- multipleOfIndexSolution.js
+++ multipleOfIndexSolution.js
@@ -1,8 +1,8 @@
 function multipleOfIndex(array) {
-    let finalArray = [];
-    for(let i = 0; i < array.length; i++){
-      if(array[i] % i === 0){
-        finalArray.push(array[i])
+    let finalArray = [];            //make an empty array var
+    for(let i = 0; i < array.length; i++){      //loop thru the input array
+      if(array[i] % i === 0){       //if each index is divisiable by the current index
+        finalArray.push(array[i])   //push it into the final array
       }
     }
     return finalArray
