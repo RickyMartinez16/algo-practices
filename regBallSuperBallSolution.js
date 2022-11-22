3221e40f6d0644e86e802ed0e0b59e04e7b8e9ef
Ricky Martinez <RMartinezW16@gmail.com>
2022-11-22 09:19:54 -0500

    ball type class final

--- regBallSuperBallSolution.js
+++ regBallSuperBallSolution.js
@@ -6,3 +6,10 @@ var Ball = function(ballType) {
     }
     return new Ball().ballType(ballType)
   };
+
+
+  //working//
+
+  var Ball = function(ballType) {
+    this.ballType = ballType || 'regular';      //when using classes THIS keyword. this ballType is given ballType or regular if balltype empty
+  };
