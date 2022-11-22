327cdf2eb3c18680400dbf8d92f80aba80b403ee
Ricky Martinez <RMartinezW16@gmail.com>
2022-11-22 09:25:10 -0500

    add length correct

--- addLengthSolution.js
+++ addLengthSolution.js
@@ -1,9 +1,9 @@
 function addLength(str) {
-    let finalArray = [];
-    let splitStr = str.split(" ")
-    for(let i = 0; i < splitStr.length; i++){
-      finalArray.push(splitStr[i] + " " +  String(splitStr[i].length))
+    let finalArray = [];        //make an empty array var
+    let splitStr = str.split(" ")   //split the input string to indiviual words
+    for(let i = 0; i < splitStr.length; i++){       //loop thru the split array
+      finalArray.push(splitStr[i] + " " +  String(splitStr[i].length))      //push the string plus the lenfth of each word into final
     }
-    return finalArray
+    return finalArray       //return finalarray
   }
   
