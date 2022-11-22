6fb0c2a9306924c8c5b34de0795d4a7ba47ef657
Ricky Martinez <RMartinezW16@gmail.com>
2022-11-21 20:27:58 -0500

    no odd comments

--- noOddSolution.js
+++ noOddSolution.js
@@ -1,9 +1,9 @@
 function noOdds( values ){
-    let evenArray = [];
-    for(let i = 0; i < values.length; i++){
-      if(values[i] % 2 == 0){
-        evenArray.push(values[i])
+    let evenArray = [];     //make an empty array to hold the even numbers
+    for(let i = 0; i < values.length; i++){     //loop thru the input array
+      if(values[i] % 2 == 0){       //if the numbers remainder is 0 when divded by 2
+        evenArray.push(values[i])       //push the number into the even array
       }
     }
-    return evenArray
+    return evenArray        //return even array
   }
