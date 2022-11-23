0dd299b9274ca34c08b8ebac78b8e13291249c02
Ricky Martinez <RMartinezW16@gmail.com>
2022-11-23 08:56:15 -0500

    filter geese

---
+++ filterGeeseSolution.js
@@ -0,0 +1,12 @@
+//------not working-----//
+
+function gooseFilter (birds) {
+    var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+    
+    // return an array containing all of the strings in the input array except those that match strings in geese
+    return birds.filter((e) => e === geese)
+  };
+
+
+//------working------//
+
