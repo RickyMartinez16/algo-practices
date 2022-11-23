f3d334a75bde6b488342a80208cafee098797332
Ricky Martinez <RMartinezW16@gmail.com>
2022-11-23 08:48:26 -0500

    bumps in the road best comments

--- bumpsInTheRoadSolution.js
+++ bumpsInTheRoadSolution.js
@@ -18,3 +18,7 @@ function bump(x){
   //----working----//
 
   const bump=x=>x.split('n').length>16?"Car Dead":"Woohoo!"     //split the input by "n"s. if the length of the array is over 16 return car dead else return woohoo
+
+  const bump = x => x.split``.filter(e => e === 'n').length > 15 ? 'Car Dead' : 'Woohoo!';
+
+  //split input to indivual letters, then filter it. if element equals n and the filter is greater than 15 return car dead else return wooho
