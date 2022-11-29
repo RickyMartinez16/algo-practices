4d9236649aeae490f771ee8c120fbfc67754e92d
Ricky Martinez <RMartinezW16@gmail.com>
2022-11-28 22:02:37 -0500

    remove exlmcations end comments

--- removeExclamationsFromEndSolution.js
+++ removeExclamationsFromEndSolution.js
@@ -1,10 +1,10 @@
 // working//
 
 function remove (string) {  
-    if(string[string.length - 1] === "!"){
+    if(string[string.length - 1] === "!"){      //if the end of the string is an exlmcation replace all of the ending exlmcations with nothing
       return string.replace(/\!+$/, "")
     } else {
-      return string
+      return string     //else return thestring
     }
   }
 
