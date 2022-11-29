d34bd6bc1ed62ff5b1c78815527e89d0d48dff94
Ricky Martinez <RMartinezW16@gmail.com>
2022-11-28 21:59:30 -0500

    removeexlcmations from end

---
+++ removeExclamationsFromEndSolution.js
@@ -0,0 +1,13 @@
+//not working//
+
+function remove (string) {  
+    if(string[string.length - 1] === "!"){
+      return string.replace(/\!+/, "")
+    } else {
+      return string
+    }
+  }
+
+  //working//
+
+const remove = s => s.replace(/!+$/, '');
