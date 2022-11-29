d93abfe0a23677e6c70f6f406e55f3a383eb3fd9
Ricky Martinez <RMartinezW16@gmail.com>
2022-11-28 21:26:04 -0500

    max sub array sum

---
+++ maximumSubarraySumSolution.js
@@ -0,0 +1,13 @@
+var maxSequence = function(arr){
+    var min = 0, ans = 0, i, sum = 0;  //make min var, answer var, i var and sum var
+
+    for (i = 0; i < arr.length; i++) {      //loop thru the array
+      sum += arr[i];    //let sum equal plus each number
+      min = Math.min(sum, min);     //min becomes the minium between sum and min
+      ans = Math.max(ans, sum - min);   //answer is the largest number between answer and sum - min
+    }
+    return ans;
+  }
+
+  /* [-2, 1, -3, 4, -1, 2, 1, -5, 4] */
+  /* [4, -1, 2, 1] = 6 */
