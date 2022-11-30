20bab465b8d06cfa95baaf194a2a2b44499b1178
Ricky Martinez <RMartinezW16@gmail.com>
2022-11-29 19:09:36 -0500

    fill array directions

--- fillArray.js
+++ fillArray.js
@@ -1,1 +1,5 @@
 var websites = new Array(1000).fill("codewars");        //fill a new array 1000 times with codewars
+
+// This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
+
+// Add the value "codewars" to the array websites/Websites 1,000 times.
