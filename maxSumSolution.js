c6ef5fa5f4c670322f181a1b8bdb88da45682439
Ricky Martinez <RMartinezW16@gmail.com>
2022-11-29 19:29:09 -0500

    max sum

--- maxSumSolution.js
+++ maxSumSolution.js
@@ -16,3 +16,11 @@ function adjacentElementsProduct(array) {
 
 
   //-------working------//
+
+  function adjacentElementsProduct(array) {
+    let newArr = []
+    for(let i=0; i < array.length-1; i++){
+      newArr.push(array[i]*array[i+1])
+    }  
+    return Math.max(...newArr)
+  }
