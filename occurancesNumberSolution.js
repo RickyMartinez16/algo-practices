440d9843889fa736e340cc6a083844dd72d1a904
Ricky Martinez <RMartinezW16@gmail.com>
2022-12-01 19:36:07 -0500

    occurances

--- occurancesNumberSolution.js
+++ occurancesNumberSolution.js
@@ -1,7 +1,7 @@
 function deleteNth(arr,x){
-    var obj = {}
-    return arr.filter(function(number){
+    var obj = {}        //make an object
+    return arr.filter(function(number){     //filter thru the input array. take each number. if it exists return obj[number] plus 1. if not make it 1
       obj[number] = obj[number] ? obj[number] + 1 : 1
-      return obj[number] <= x
+      return obj[number] <= x       //return obj number only if its less than or equal to x in the filter
     })
   }
