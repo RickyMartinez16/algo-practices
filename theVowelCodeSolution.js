5c2c07e4c898be6c888843359ad3b364499cfa11
Ricky Martinez <RMartinezW16@gmail.com>
2022-12-01 19:13:24 -0500

    the vowel code

--- theVowelCodeSolution.js
+++ theVowelCodeSolution.js
@@ -11,3 +11,20 @@ function encode(string){
   }
 
   //return a strin replaceing all the nums with the charAt x
+
+
+  function encode(string){
+    var vowelMapping = {'a': 1, 'e': 2, 'i': 3, 'o': 4, 'u': 5};        //make a vowel map
+    return codeStringGivenMapping(string, vowelMapping); //use helper function
+  }
+  
+  function decode(string){
+    var vowelMapping = {'1': 'a', '2': 'e', '3': 'i', '4': 'o', '5': 'u'};      //make a number map
+    return codeStringGivenMapping(string, vowelMapping);    //return helper function
+  }
+  
+  function codeStringGivenMapping(string, mapping){     //helper function that takes the string and map
+    return string.split('').map(function(char){     //split the string then map over it. take each character and return the mapped char or character if none avail.
+      return mapping[char] || char;
+    }).join('');    //then join
+  }
