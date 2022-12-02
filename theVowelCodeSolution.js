acc236c7b24386a11b934c10a36f96dad1db981d
Ricky Martinez <RMartinezW16@gmail.com>
2022-12-01 19:10:24 -0500

    the vowel code

--- theVowelCodeSolution.js
+++ theVowelCodeSolution.js
@@ -2,8 +2,12 @@
 function encode(string){
     return string.replace(/[aeiou]/g, function (x) { return '_aeiou'.indexOf(x) });
   }
+
+  //return a strin replaceing all the vowels with the index of x
   
   //turn numbers back into vowels
   function decode(string){
     return string.replace(/[1-5]/g, function (x) { return '_aeiou'.charAt(x) });
   }
+
+  //return a strin replaceing all the nums with the charAt x
