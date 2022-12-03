c3b2df3ff12a6e0677959182de7c9950b914ef80
Ricky Martinez <RMartinezW16@gmail.com>
2022-12-03 14:03:39 -0500

    count letters comments

--- countLettersSolution.js
+++ countLettersSolution.js
@@ -1,9 +1,9 @@
 function strCount(str, letter){  
-    let count = 0
-    for(let char of str){
-      if (char === letter){
-        count++
+    let count = 0       //make a count var
+    for(let char of str){       //loop thru string
+      if (char === letter){     //if char equals letter
+        count++     //add one to the count
       }
     }
-    return count
+    return count        //return the count
   }
