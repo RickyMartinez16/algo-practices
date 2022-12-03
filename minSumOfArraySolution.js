92cf44dfc284d9d25a6306125caa8e22b83b78ca
Ricky Martinez <RMartinezW16@gmail.com>
2022-12-03 14:58:09 -0500

    min sum comments

--- minSumOfArraySolution.js
+++ minSumOfArraySolution.js
@@ -1,24 +1,23 @@
 function minSum(arr) {
     //biggest times smallest
     
-    let newArray = [];
-    let sortedArray = arr.sort((a, b) => a - b)
+    let newArray = [];      //make a new arrat var
+    let sortedArray = arr.sort((a, b) => a - b)     //sort the input array
     
-    let j = sortedArray.length - 1
-    for(let i = 0; i < sortedArray.length; i++){
+    let j = sortedArray.length - 1      //make a j var
+    for(let i = 0; i < sortedArray.length; i++){        //loop thru sorted array
       
       
-      let num = sortedArray[i] * sortedArray[j]
-      console.log(sortedArray[i], sortedArray[j])
+      let num = sortedArray[i] * sortedArray[j] //find a number to push into new array. smallest times biggest
+    
+      newArray.push(num);   //pish the product into the new array
       
-      newArray.push(num);
-      
-      j--
-      if(newArray.length == sortedArray.length /2) break
+      j--   //interate j to minus 1
+      if(newArray.length == sortedArray.length /2) break    //if the array length is the same as half the array break 
     }
     const sum = newArray.reduce(
       (accumulator, currentValue) => accumulator + currentValue,
       0
     );
-    return sum
+    return sum  //reduce the new array and return
   }
