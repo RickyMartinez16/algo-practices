595d1f0ab98388de61abfcf0e90f500483e1fe0a
Ricky Martinez <RMartinezW16@gmail.com>
2022-12-03 13:23:59 -0500

    regex count lowercase

--- regexCountLowerCaseSolution.js
+++ regexCountLowerCaseSolution.js
@@ -1,9 +1,9 @@
 function lowercaseCount(str){
-    let count = 0;
-  for(let letter of str){
-    if(letter.match(/^[a-z]/g)){
-       count++
+    let count = 0;      //make a count var
+  for(let letter of str){       //loop thru the string
+    if(letter.match(/^[a-z]/g)){        //if a letter matches a lowercase regex
+       count++      //add one to the count
        }
   }
-  return count
+  return count      //return count
 }
