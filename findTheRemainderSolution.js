9be24b4aa1c6086c29bbd89ae0f720f56ee1e874
Ricky Martinez <RMartinezW16@gmail.com>
2022-12-04 18:58:59 -0500

    find the raminer comms

--- findTheRemainderSolution.js
+++ findTheRemainderSolution.js
@@ -1,3 +1,5 @@
 function remainder(a, b){
     return a > b ? a % b : b % a;   //if a is greater than b return a modelo b, else return b modelo a
   }
+
+  //need to make sure youre dividing the bigger number with the smalelr number
