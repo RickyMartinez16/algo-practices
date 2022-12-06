05e3ababbb2c1a71c05e75ad42fe58d096c4c02e
Ricky Martinez <RMartinezW16@gmail.com>
2022-12-05 20:20:55 -0500

    move in swaure string

--- moveInSquareStringsSolution.js
+++ moveInSquareStringsSolution.js
@@ -1,7 +1,7 @@
 function vertMirror(str) {
-    return str.split('\n')
-      .map(line => line.split('').reverse().join(''))
-      .join('\n')
+    return str.split('\n')  //split the input by the spaces.
+      .map(line => line.split('').reverse().join(''))   //map the line. split each line then reverse it then join it again
+      .join('\n')   //join the lines by the \
 }
 function horMirror(str) {
     return str.split('\n')
