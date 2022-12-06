456e936aa2c47d6eaab7c5a3937025b16c651e08
Ricky Martinez <RMartinezW16@gmail.com>
2022-12-05 20:11:13 -0500

    peristence

--- persistentBuggerSolution.js
+++ persistentBuggerSolution.js
@@ -26,3 +26,20 @@ function persistence(num) {
     
     return count
   }
+
+
+
+  //-------working-----//
+
+  function persistence(num) {
+    var times = 0;
+    
+    num = num.toString();
+    
+    while (num.length > 1) {
+      times++;
+      num = num.split('').map(Number).reduce((a, b) => a * b).toString();
+    }
+    
+    return times;
+ }
