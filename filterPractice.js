2c759508ad57293b59661610413234bdb6bfbbf7
Ricky Martinez <RMartinezW16@gmail.com>
2022-12-12 08:14:50 -0500

    filter practice word length

--- filterPractice.js
+++ filterPractice.js
@@ -11,8 +11,19 @@
 
 // Given an array of numbers, return a new array that only includes the even numbers.
 
-function evensOnly(arr) {
-    return arr.filter(num => num % 2 == 0)
+// function evensOnly(arr) {
+//     return arr.filter(num => num % 2 == 0)
+//   }
+//   // test
+//   console.log(evensOnly([3, 6, 8, 2])); /// [6, 8, 2]
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------//
+
+//3) Given an array of strings, return a new array that only includes those that are 5 characters or fewer in length
+
+function fiveCharactersOrFewerOnly(arr) {
+    return arr.filter(word => word.length <= 5)
   }
   // test
-  console.log(evensOnly([3, 6, 8, 2])); /// [6, 8, 2]
+  console.log(fiveCharactersOrFewerOnly(["dog", "wolf", "by", "family", "eaten", "camping"])); // ["by", "dog", "wolf", "eaten"]
