cc01940d455ea8ac5548739c19e348e516133833
Ricky Martinez <RMartinezW16@gmail.com>
2022-12-12 07:54:56 -0500

    map practice list of names object

--- mapPractice.js
+++ mapPractice.js
@@ -22,8 +22,43 @@
 
 //Capitalize each of an array of names
 
-function capitalizeNames(arr){
-    return arr.map((name) => name.charAt(0).toUpperCase() + name.slice(1).toLowerCase())
+// function capitalizeNames(arr){
+//     return arr.map((name) => name.charAt(0).toUpperCase() + name.slice(1).toLowerCase())
+//   }
+  
+//   console.log(capitalizeNames(["john", "JACOB", "jinGleHeimer", "schmidt"])); // ["John", "Jacob", "Jingleheimer", "Schmidt"]
+
+
+//--------------------------------------------------------------------------------------------------------------------------------------//
+
+//4) Make an array of strings of the names
+
+function namesOnly(arr){
+    
+    // return arr.map(namesOnly.name)
+    return arr.map(object => object.name);  //need to map of the obect and return the object.name
   }
   
-  console.log(capitalizeNames(["john", "JACOB", "jinGleHeimer", "schmidt"])); // ["John", "Jacob", "Jingleheimer", "Schmidt"]
+  console.log(namesOnly([
+      {
+          name: "Angelina Jolie",
+          age: 80
+      },
+      {
+          name: "Eric Jones",
+          age: 2
+      },
+      {
+          name: "Paris Hilton",
+          age: 5
+      },
+      {
+          name: "Kayne West",
+          age: 16
+      },
+      {
+          name: "Bob Ziroll",
+          age: 100
+      }
+  ])); 
+  // ["Angelina Jolie", "Eric Jones", "Paris Hilton", "Kayne West", "Bob Ziroll"]
