40dc338358e71f110c36f39bdb239d196d90b1db
Ricky Martinez <RMartinezW16@gmail.com>
2022-12-12 08:01:17 -0500

    map practice can theygo to the matrix

--- mapPractice.js
+++ mapPractice.js
@@ -33,13 +33,46 @@
 
 //4) Make an array of strings of the names
 
-function namesOnly(arr){
+// function namesOnly(arr){
     
-    // return arr.map(namesOnly.name)
-    return arr.map(object => object.name);  //need to map of the obect and return the object.name
+//     // return arr.map(namesOnly.name)
+//     return arr.map(object => object.name);  //need to map of the obect and return the object.name
+//   }
+  
+//   console.log(namesOnly([
+//       {
+//           name: "Angelina Jolie",
+//           age: 80
+//       },
+//       {
+//           name: "Eric Jones",
+//           age: 2
+//       },
+//       {
+//           name: "Paris Hilton",
+//           age: 5
+//       },
+//       {
+//           name: "Kayne West",
+//           age: 16
+//       },
+//       {
+//           name: "Bob Ziroll",
+//           age: 100
+//       }
+//   ])); 
+//   // ["Angelina Jolie", "Eric Jones", "Paris Hilton", "Kayne West", "Bob Ziroll"]
+
+
+  //-----------------------------------------------------------------------------------------------------------------------------------//
+
+//   5) Make an array of strings of the names saying whether or not they can go to The Matrix
+
+function makeStrings(arr){
+    return arr.map(object => object.age > 20 ? `${object.name} can go to The Matrix` : `${object.name} is under age!!`)
   }
   
-  console.log(namesOnly([
+  console.log(makeStrings([
       {
           name: "Angelina Jolie",
           age: 80
@@ -61,4 +94,8 @@ function namesOnly(arr){
           age: 100
       }
   ])); 
-  // ["Angelina Jolie", "Eric Jones", "Paris Hilton", "Kayne West", "Bob Ziroll"]
+  // ["Angelina Jolie can go to The Matrix", 
+  // "Eric Jones is under age!!", 
+  // "Paris Hilton is under age!!", 
+  // "Kayne West is under age!!", 
+  // "Bob Ziroll can go to The Matrix"]
