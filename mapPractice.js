1b1dd6216eaca41932e1cf9335cdfbce79e25cc8
Ricky Martinez <RMartinezW16@gmail.com>
2022-12-12 08:05:55 -0500

    nap practice h1 to name and object

--- mapPractice.js
+++ mapPractice.js
@@ -68,11 +68,47 @@
 
 //   5) Make an array of strings of the names saying whether or not they can go to The Matrix
 
-function makeStrings(arr){
-    return arr.map(object => object.age > 20 ? `${object.name} can go to The Matrix` : `${object.name} is under age!!`)
-  }
+// function makeStrings(arr){
+//     return arr.map(object => object.age > 20 ? `${object.name} can go to The Matrix` : `${object.name} is under age!!`)
+//   }
   
-  console.log(makeStrings([
+//   console.log(makeStrings([
+//       {
+//           name: "Angelina Jolie",
+//           age: 80
+//       },
+//       {
+//           name: "Eric Jones",
+//           age: 2
+//       },
+//       {
+//           name: "Paris Hilton",
+//           age: 5
+//       },
+//       {
+//           name: "Kayne West",
+//           age: 16
+//       },
+//       {
+//           name: "Bob Ziroll",
+//           age: 100
+//       }
+//   ])); 
+//   // ["Angelina Jolie can go to The Matrix", 
+//   // "Eric Jones is under age!!", 
+//   // "Paris Hilton is under age!!", 
+//   // "Kayne West is under age!!", 
+//   // "Bob Ziroll can go to The Matrix"]
+
+
+//------------------------------------------------------------------------------------------------------------------------------------//
+
+// 6) Make an array of the names in h1s, and the ages in h2s
+
+function readyToPutInTheDOM(arr){
+    return arr.map(object => `<h1>${object.name}</h1><h2>${object.age}</h2>`)
+  }
+  console.log(readyToPutInTheDOM([
       {
           name: "Angelina Jolie",
           age: 80
@@ -94,8 +130,8 @@ function makeStrings(arr){
           age: 100
       }
   ])); 
-  // ["Angelina Jolie can go to The Matrix", 
-  // "Eric Jones is under age!!", 
-  // "Paris Hilton is under age!!", 
-  // "Kayne West is under age!!", 
-  // "Bob Ziroll can go to The Matrix"]
+  // ["<h1>Angelina Jolie</h1><h2>80</h2>", 
+  // "<h1>Eric Jones</h1><h2>2</h2>", 
+  // "<h1>Paris Hilton</h1><h2>5</h2>", 
+  // "<h1>Kayne West</h1><h2>16</h2>", 
+  // "<h1>Bob Ziroll</h1><h2>100</h2>"]
