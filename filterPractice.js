bfef50b925f16d08ab17fde4ba1be7f1a8c7c8a9
Ricky Martinez <RMartinezW16@gmail.com>
2022-12-12 08:11:29 -0500

    filter practice only evens

--- filterPractice.js
+++ filterPractice.js
@@ -1,7 +1,18 @@
 // 1) Given an array of numbers, return a new array that has only the numbers that are 5 or greater.
 
-function fiveAndGreaterOnly(arr) {
-    return arr.filter(num => num > 5)
+// function fiveAndGreaterOnly(arr) {
+//     return arr.filter(num => num > 5)
+//   }
+//   // test
+//   console.log(fiveAndGreaterOnly([3, 6, 8, 2])); /// [6, 8]
+
+
+//------------------------------------------------------------------------------------------------------------------------------------//
+
+// Given an array of numbers, return a new array that only includes the even numbers.
+
+function evensOnly(arr) {
+    return arr.filter(num => num % 2 == 0)
   }
   // test
-  console.log(fiveAndGreaterOnly([3, 6, 8, 2])); /// [6, 8]
+  console.log(evensOnly([3, 6, 8, 2])); /// [6, 8, 2]
