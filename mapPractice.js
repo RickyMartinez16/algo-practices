e801b3917993fdff26ab320b74938d3a1f45b89a
Ricky Martinez <RMartinezW16@gmail.com>
2022-12-12 07:30:04 -0500

    num to string map

--- mapPractice.js
+++ mapPractice.js
@@ -1,7 +1,18 @@
 // Make an array of numbers that are doubles of the first array
 
-function doubleNumbers(arr){
-    return arr.map((num) => num * 2)
+// function doubleNumbers(arr){
+//     return arr.map((num) => num * 2)
+//   }
+  
+//   console.log(doubleNumbers([2, 5, 100])); // [4, 10, 200]
+
+
+//------------------------------------------------------------------------------------------------------------------//
+
+// Take an array of numbers and make them strings
+
+function stringItUp(arr){
+    return arr.map((num) => String(num))
   }
   
-  console.log(doubleNumbers([2, 5, 100])); // [4, 10, 200]
+  console.log(stringItUp([2, 5, 100])); // ["2", "5", "100"]
