f570dbce8f0ecfe8565d3bf02279c5218a3cba6a
Ricky Martinez <RMartinezW16@gmail.com>
2022-12-13 07:27:38 -0500

    map practice

--- mapPractice.js
+++ mapPractice.js
@@ -105,33 +105,49 @@
 
 // 6) Make an array of the names in h1s, and the ages in h2s
 
-function readyToPutInTheDOM(arr){
-    return arr.map(object => `<h1>${object.name}</h1><h2>${object.age}</h2>`)
-  }
-  console.log(readyToPutInTheDOM([
-      {
-          name: "Angelina Jolie",
-          age: 80
-      },
-      {
-          name: "Eric Jones",
-          age: 2
-      },
-      {
-          name: "Paris Hilton",
-          age: 5
-      },
-      {
-          name: "Kayne West",
-          age: 16
-      },
-      {
-          name: "Bob Ziroll",
-          age: 100
-      }
-  ])); 
-  // ["<h1>Angelina Jolie</h1><h2>80</h2>", 
-  // "<h1>Eric Jones</h1><h2>2</h2>", 
-  // "<h1>Paris Hilton</h1><h2>5</h2>", 
-  // "<h1>Kayne West</h1><h2>16</h2>", 
-  // "<h1>Bob Ziroll</h1><h2>100</h2>"]
+// function readyToPutInTheDOM(arr){
+//     return arr.map(object => `<h1>${object.name}</h1><h2>${object.age}</h2>`)
+//   }
+//   console.log(readyToPutInTheDOM([
+//       {
+//           name: "Angelina Jolie",
+//           age: 80
+//       },
+//       {
+//           name: "Eric Jones",
+//           age: 2
+//       },
+//       {
+//           name: "Paris Hilton",
+//           age: 5
+//       },
+//       {
+//           name: "Kayne West",
+//           age: 16
+//       },
+//       {
+//           name: "Bob Ziroll",
+//           age: 100
+//       }
+//   ])); 
+//   // ["<h1>Angelina Jolie</h1><h2>80</h2>", 
+//   // "<h1>Eric Jones</h1><h2>2</h2>", 
+//   // "<h1>Paris Hilton</h1><h2>5</h2>", 
+//   // "<h1>Kayne West</h1><h2>16</h2>", 
+//   // "<h1>Bob Ziroll</h1><h2>100</h2>"]
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------//
+
+// Square the value of every element in the array. Presume that you will only get numbers in the input array.
+
+// const input = [1, 2, 3, 4, 5];
+
+// function square(array){
+//     return array.map((num) => Math.pow(num, 2))
+// }
+
+// console.log(square(input))
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------//
