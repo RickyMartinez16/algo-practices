234eb9182d64e7912b108caabe4e44ce3ad40266
Ricky Martinez <RMartinezW16@gmail.com>
2022-12-13 11:21:07 -0500

    duplcate arrow func

--- duplicateEncode.js
+++ duplicateEncode.js
@@ -2,9 +2,9 @@ function duplicateEncode(word){
     return word     
       .toLowerCase()    //make the input word lowercase
       .split('')    //split the word to get each letter
-      .map( function (a, i, w) {            //map thru the word
-        return w.indexOf(a) == w.lastIndexOf(a) ? '(' : ')'     //return either ( or ) depending on indexOf and lastIndexOf. 
-      })
+      .map((letter, i, w) =>             //map thru the word
+        w.indexOf(letter) == w.lastIndexOf(letter) ? '(' : ')'     //return either ( or ) depending on indexOf and lastIndexOf. 
+      )
       .join('');    //join the map
   }
 
