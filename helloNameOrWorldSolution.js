01ac851d1cf78fc1a9606f18ee1e2b6e9c8ac1e9
Ricky Martinez <RMartinezW16@gmail.com>
2022-12-14 08:26:03 -0500

    hellow name or world

--- helloNameOrWorldSolution.js
+++ helloNameOrWorldSolution.js
@@ -1,7 +1,7 @@
 function hello(name) {
-    if(name){
-      name = name.toLowerCase()
-      return `Hello, ${name.charAt(0).toUpperCase() + name.slice(1)}!`
+    if(name){       //if name input exists
+      name = name.toLowerCase() //make the name all lower case
+      return `Hello, ${name.charAt(0).toUpperCase() + name.slice(1)}!`  //then return the first letter as capps and the rest 
     }
-    return "Hello, World!"
+    return "Hello, World!"      //else return hellow world"
   }
