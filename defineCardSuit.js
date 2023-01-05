3dbef45a0962d8969f214a9ff855f0f2abe4eadb
Ricky Martinez <RMartinezW16@gmail.com>
2023-01-05 09:54:32 -0500

    define the suit

---
+++ defineCardSuit.js
@@ -0,0 +1,30 @@
+// You get any card as an argument. Your task is to return the suit of this card (in lowercase).
+
+// Our deck (is preloaded):
+
+// ('3♣') -> return 'clubs'
+// ('3♦') -> return 'diamonds'
+// ('3♥') -> return 'hearts'
+// ('3♠') -> return 'spades'
+
+
+function defineSuit(card) {
+    //make object with key value pair of suit and name of suit
+    const s = {
+      "♣": "clubs",
+      "♠": "spades",
+      "♦": "diamonds",
+      "♥": "hearts"
+    }
+    return s[card.charAt(card.length - 1)]  //return the object value at the last character of the card
+  }
+
+
+
+  function defineSuit(card) {
+      //if the card includes a specific icon return that suit
+    if(card.includes('♥')) return 'hearts'
+    if(card.includes('♦')) return 'diamonds'
+    if(card.includes('♣')) return 'clubs'
+    if(card.includes('♠')) return 'spades' 
+    }
