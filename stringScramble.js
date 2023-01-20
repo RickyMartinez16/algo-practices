0313416762f1dcf65a6b4b3f69bf73912cfc178a
Ricky Martinez <RMartinezW16@gmail.com>
2023-01-19 19:49:34 -0500

    working scramble

--- stringScramble.js
+++ stringScramble.js
@@ -12,6 +12,9 @@
 
 
 function scramble(str, arr) {
-    for(var r=[],i=0;i<arr.length;i++) r[arr[i]]=str[i]
+    //loop thru arr
+    for(var r = [], i=0; i<arr.length; i++) {
+        r[arr[i]]=str[i]
+    }
     return r.join("")
   };
