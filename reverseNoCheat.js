e205ceb9db2698192f48ebe05f29c4d5752927a9
Ricky Martinez <RMartinezW16@gmail.com>
2023-01-19 19:13:16 -0500

    reverse no cheating comments

--- reverseNoCheat.js
+++ reverseNoCheat.js
@@ -4,8 +4,12 @@
 
 
 reverse = function(array) {
+    //make an empty array var
     let finalArray = [];
+    
+    //loop thru the input array. you want to work backwards so start i at the end and work down to get reverse roder
     for(let i = array.length - 1; i >= 0 ; i--){
+        //push into final array
       finalArray.push(array[i])
     }
     return finalArray
