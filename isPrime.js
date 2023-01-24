ad4b925bad0f1fd9a74356b28741d1dbbed853b5
Ricky Martinez <RMartinezW16@gmail.com>
2023-01-24 17:35:59 -0500

    prime comments

--- isPrime.js
+++ isPrime.js
@@ -21,7 +21,10 @@ function isPrime(num) {
 
     //check if number is even or can be devivded by 3 and return false 
     if (num % 2 == 0 || num % 3 == 0) return false;
+
+    // Then it uses a for loop to iterate through all the numbers between 5 and the square root of the input number.
     for (let i = 5; i * i <= num; i = i + 6) {
+        
         if (num % i == 0 || num % (i + 2) == 0) return false;
     }
     return true;
