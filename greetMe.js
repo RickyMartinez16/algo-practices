689b1dbc22bc681d617f31fc07a79a7c52ffda0a
Ricky Martinez <RMartinezW16@gmail.com>
2023-02-06 20:20:32 -0500

    comments

--- greetMe.js
+++ greetMe.js
@@ -6,7 +6,8 @@
 // "JACK"  --> "Hello Jack!"
 
 var greet = function(name) {
+
+    //return "hello + first letter of name input turned to uppercase plus last part of name using substring to lowercase plus !"
     return "Hello " + name[0].toUpperCase() + name.substring(1, name.length).toLowerCase() + "!"
   };
 
-  
