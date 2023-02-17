44cb057b2fde499210aec23744f6ede1ec7ee129
Ricky Martinez <RMartinezW16@gmail.com>
2023-02-16 22:22:53 -0500

    not working solution

--- OOPpiracy.js
+++ OOPpiracy.js
@@ -1,6 +1,7 @@
 // Ahoy matey!
 
-// You are a leader of a small pirate crew. And you have a plan. With the help of OOP you wish to make a pretty efficient system to identify ships with heavy booty on board!
+// You are a leader of a small pirate crew. And you have a plan. 
+//With the help of OOP you wish to make a pretty efficient system to identify ships with heavy booty on board!
 
 // Unfortunately for you, people weigh a lot these days, so how do you know if a ship is full of gold and not people?
 
@@ -16,9 +17,12 @@
 // crew - the count of crew on board
 // var titanic = new Ship(15, 10);
 // Task
-// You have access to the ship "draft" and "crew". "Draft" is the total ship weight and "crew" is the number of humans on the ship.
+// You have access to the ship "draft" and "crew". 
+//"Draft" is the total ship weight and "crew" is the number of humans on the ship.
 
-// Each crew member adds 1.5 units to the ship draft. If after removing the weight of the crew, the draft is still more than 20, then the ship is worth looting. Any ship weighing that much must have a lot of booty!
+// Each crew member adds 1.5 units to the ship draft. 
+//If after removing the weight of the crew, the draft is still more than 20, 
+//then the ship is worth looting. Any ship weighing that much must have a lot of booty!
 
 // Add the method
 
@@ -28,3 +32,19 @@
 // titanic.isWorthIt() // return false
 // Good luck and may you find GOOOLD!
 
+
+
+//NOT WORKING-------------------------------
+
+function Ship(draft,crew) {
+    this.draft = draft;
+    this.crew = crew;
+   }
+   
+   function isWorthIt(ship){
+     if(ship.draft + ship.crew * 1.5 > 20){
+       return true
+     } else {
+       return false
+     }
+   }
