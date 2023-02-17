0adf5dca1b9593f0f72b0743868b9847a7c74275
Ricky Martinez <RMartinezW16@gmail.com>
2023-02-17 16:09:17 -0500

    working

--- offspring.js
+++ offspring.js
@@ -10,3 +10,8 @@
 // If the sperm contains the X chromosome, return "Congratulations! You're going to have a daughter."; 
 //If the sperm contains the Y chromosome, return "Congratulations! You're going to have a son.";
 
+
+
+function chromosomeCheck(sperm) {
+    return sperm === "XX" ? "Congratulations! You're going to have a daughter." : "Congratulations! You're going to have a son."
+  }
