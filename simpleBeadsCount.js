97cd58e0288e04904547f625bbe9ef4c1ec5d86e
Ricky Martinez <RMartinezW16@gmail.com>
2023-02-16 22:38:49 -0500

    working

--- simpleBeadsCount.js
+++ simpleBeadsCount.js
@@ -7,3 +7,8 @@
 //in Java, Javascript, TypeScript, C, C++ countRedBeads(n)) so that it returns the number of red beads.
 // If there are less than 2 blue beads return 0.
 
+function countRedBeads(n) {
+    if(n <=1) return 0
+    
+    return (n * 2) - 2
+  }
