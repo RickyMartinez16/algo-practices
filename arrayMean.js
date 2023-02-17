4eefa63051c9d311b519cdb458c3dad0cd2bfa45
Ricky Martinez <RMartinezW16@gmail.com>
2023-02-17 16:14:19 -0500

    working

--- arrayMean.js
+++ arrayMean.js
@@ -1,1 +1,12 @@
-// To find the mean (average) of a set of numbers add all of the numbers together and divide by the number of values in the list.
+// To find the mean (average) of a set of numbers add all of the numbers together and divide by the number of values in the list.\
+
+
+var findAverage = function (nums) {
+    const initialValue = 0;
+    const reduced = nums.reduce(
+      (accumulator, currentValue) => accumulator + currentValue,
+        initialValue
+    );
+    
+    return reduced / nums.length
+  }
