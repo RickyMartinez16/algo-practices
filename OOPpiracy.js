c642be74a12db670824e92c6aee88fc2ec23fd6a
Ricky Martinez <RMartinezW16@gmail.com>
2023-02-16 22:23:50 -0500

    working

--- OOPpiracy.js
+++ OOPpiracy.js
@@ -48,3 +48,14 @@ function Ship(draft,crew) {
        return false
      }
    }
+
+//WORKING------------------------------
+
+   function Ship(draft,crew) {
+    this.draft = draft;
+    this.crew = crew;
+   }
+   
+   Ship.prototype.isWorthIt = function(){
+   return this.draft-(this.crew*1.5) > 20;
+   }
