0a136e14e85d744b85edd8f870c71573b8630051
Ricky Martinez <RMartinezW16@gmail.com>
2023-02-23 13:05:16 -0500

    working

--- removeDupesFromSortedArray.js
+++ removeDupesFromSortedArray.js
@@ -11,3 +11,15 @@
 // Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
 
 
+var removeDuplicates = function(nums) {
+    if(nums.length === 0) return 0
+
+    let i = 0
+    for(let j = 1; j < nums.length; j++){
+        if(nums[i] !== nums[j]){
+            i++
+            nums[i] = nums[j]
+        }
+    }
+    return i + 1
+};
