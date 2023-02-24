251741e80046f3f278e418c73ede772b8faf3450
Ricky Martinez <RMartinezW16@gmail.com>
2023-02-24 12:05:27 -0500

    working

--- parentsValid.js
+++ parentsValid.js
@@ -21,24 +21,27 @@
 
 
 const pairs = {
-    "(" : ")",
-    "{" : "}",
-    "[" : "]"
+    "(": ")",
+    "{": "}",
+    "[": "]"
 }
 
-function validParents(s){
-    if(s[0] === ")" || "}" || "]") return false;
-    if(s.length % 2 !== 0) return false;
-    if(s[s.length - 1] == "(" || "{" || "[") return false
+function isValid(s){
+
+    if(s.length % 2 === 1) return false;
+    if(s[0] === ")" || s[0] === "}" || s[0] === "]") return false;
+    if(s[s.length - 1] == "(" || s[s.length - 1] =="{" || s[s.length - 1] =="[") return false
+    
 
     let stack = [];
 
     for(let i = 0; i < s.length; i++){
-        if(s[i] == "(" || "{" || "["){
+        if(s[i] == "(" || s[i] == "{" || s[i] == "["){
             stack.push(s[i])
         } else if (pairs[stack.pop()] !== s[i]){
             return false 
         }
     }
     return stack.length === 0
+
 }
