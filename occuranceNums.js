3b909557ee1b5eb95f1578c61d669ee8ded1e698
Ricky Martinez <RMartinezW16@gmail.com>
2023-02-25 14:18:13 -0500

    woeking iwth comments

--- occuranceNums.js
+++ occuranceNums.js
@@ -1,12 +1,18 @@
 let input = [1,2,3,4,2,3,2,4,2,1,2,3,4,5,6,4,6,5,3,5,7,8,5,4,3,2,3,4,5,6,7,5,4,3,4,5,6,7,8,9,8,6,5,6,7,8,3,5,6,7,8,0,1]
 
 function occuranceNums(array){
+
+    //create a map
     let map = {}
 
+    //loop thru input array
     for(let i = 0; i < array.length; i++){
+        //if key exists in the map
         if(map[array[i]]){
+            //add one to its value
             map[array[i]]++
         } else {
+            //if it doesnt exist already then make it start at 1
             map[array[i]] = 1
         }
     }
