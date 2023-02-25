ee75fe109d57bfeb035a8c2fc1e5d00ab0cff46f
Ricky Martinez <RMartinezW16@gmail.com>
2023-02-25 10:00:59 -0500

    working with comments

--- occurancesAgain.js
+++ occurancesAgain.js
@@ -1,17 +1,21 @@
 const input = "all day i dream about sports"
 
 function occurrences(string){
+    //make a map
     let map = {}
 
-    // let split = array.split("")
-
+    //loop thru the string input
     for(let i = 0; i < string.length; i++){
+        //if the key exists in the map
         if(map[string[i]]){
+            //add one to the value
             map[string[i]]++
         } else {
+            //if it doesnt already exist then make it start at 1
             map[string[i]] = 1
         }
     }
+    //return the map
     return map
 }
 
