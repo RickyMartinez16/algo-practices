fad3a6e051a03be18e410c7ea167323a77930cf0
Ricky Martinez <RMartinezW16@gmail.com>
2023-02-28 17:19:42 -0500

    final

--- removeEveryOtherSolution.js
+++ removeEveryOtherSolution.js
@@ -4,5 +4,6 @@ function removeEveryOther(arr) {
   for (let i = 0; i < arr.length; i = i + 2) {  //loop thru the array and skip every other
     finalArray.push(arr[i]);        //push every other into final array 
   }
+  //return final
   return finalArray;
 }
