bb637d7fbbca9193081851f1e3e37379fab8b0a4
Ricky Martinez <RMartinezW16@gmail.com>
2023-03-01 17:16:21 -0500

    comments

--- removeDupsFromLinkedList.js
+++ removeDupsFromLinkedList.js
@@ -13,16 +13,23 @@
  * @return {ListNode}
  */
  var deleteDuplicates = function(head) {
+     //if the head or the head next is null 
     if(!head || !head.next){
+        //return the head
         return head
     }
 
+    //make a var for the current
     let current = head
 
+    //while the current is not null and the current next is not full
     while(current != null && current.next != null){
+        //check if the current value equals the next value
         if(current.val === current.next.val){
+            //if yes then the current next becomes the current next next. then check again
             current.next = current.next.next
         } else {
+            //if it isntthe same then the current pointer becomes the next because its already sorted
             current = current.next
         }
     }
