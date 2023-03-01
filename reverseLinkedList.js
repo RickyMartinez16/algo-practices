a73935e5e5547fd2043ade4dffdcf8355a4539d3
Ricky Martinez <RMartinezW16@gmail.com>
2023-03-01 18:55:19 -0500

    working comments

--- reverseLinkedList.js
+++ reverseLinkedList.js
@@ -21,15 +21,23 @@
  * @return {ListNode}
  */
  var reverseList = function(head) {
+     //make 3 lists. prev, current and next
     var prev = null;
     var current = head;
     var next = null;
+
+    //while current is not null
         while (current != null) {
+            //next is currents next
             next = current.next;
+            //current next becomes prev (null to start)
             current.next = prev;
+            //prev is what current was
             prev = current;
+            //currnent is now next
             current = next;
         }
+        //head is prev final
         head = prev;
         return head;
 };
