0d493d040a7d89be596d7b20e7784eaf0a6e1600
Ricky Martinez <RMartinezW16@gmail.com>
2023-03-02 16:38:03 -0500

    not working

--- singleNumber.js
+++ singleNumber.js
@@ -16,3 +16,20 @@
 
 // Input: nums = [1]
 // Output: 1
+
+
+var singleNumber = function(nums) {
+    let map = {}
+
+    for(let i = 0; i < nums.length; i++){
+        if(!map[nums[i]]){
+            map[nums[i]] = 1
+        } else {
+            map[nums[i]]++
+        }
+    }
+
+    if(map[nums] === 1){
+        return map[nums]
+    }
+};
