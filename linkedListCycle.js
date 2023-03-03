1dea8045122e648c1467833973b699eebdbac92d
Ricky Martinez <RMartinezW16@gmail.com>
2023-03-03 13:02:08 -0500

    working

--- linkedListCycle.js
+++ linkedListCycle.js
@@ -26,3 +26,29 @@
 // Input: head = [1], pos = -1
 // Output: false
 // Explanation: There is no cycle in the linked list.
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+ var hasCycle = function(head) {
+    let fast = head
+
+    while(fast && fast.next){
+        head = head.next;
+        fast = fast.next.next
+        if(head === fast){
+        return true
+    }
+    }
+    return false
+};
