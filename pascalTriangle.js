354b0ef485553222e5c7495faed57e06329e6f67
Ricky Martinez <RMartinezW16@gmail.com>
2023-03-15 15:36:44 -0400

    comments

--- pascalTriangle.js
+++ pascalTriangle.js
@@ -11,13 +11,22 @@
 // Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
 
 var generate = function(numRows) {
+    //make a final solution return
     var pascal = [];
+
+    //loop the numRows amount
     for (var i = 0; i < numRows; i++) {
+        //put a new array into the pascal array
         pascal[i] = [];
+        //give the first space in the new array in the pascal array a value of 1
         pascal[i][0] = 1;
+
+        //loop i amount of times
         for (var j = 1; j < i; j++) {
+            //make the next amount in the array in the array to be this 
             pascal[i][j] = pascal[i-1][j-1] + pascal[i-1][j]
         }
+        //make next array start a 1
         pascal[i][i] = 1;
     }
     return pascal;
