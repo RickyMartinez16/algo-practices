325f13ced47f05e6290a89e45d25f8811bf84f59
Ricky Martinez <RMartinezW16@gmail.com>
2023-03-15 15:48:13 -0400

    directions

--- binaryTreeSearchPostorder.js
+++ binaryTreeSearchPostorder.js
@@ -1,3 +1,16 @@
+// Given the root of a binary tree, return the postorder traversal of its nodes' values.
+
+ 
+
+// Example 1:
+
+
+// Input: root = [1,null,2,3]
+// Output: [3,2,1]
+
+
+
+
 function postorderTraversal(root) {
     if(!root) {
         return [];
