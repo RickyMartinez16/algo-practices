45d636123a29f47c44b7f4027f42579198aa2d0f
Ricky Martinez <RMartinezW16@gmail.com>
2023-03-15 15:52:33 -0400

    final working comments

--- binaryTreeSearchPostorder.js
+++ binaryTreeSearchPostorder.js
@@ -15,21 +15,32 @@ function postorderTraversal(root) {
         return [];
     }
 
+    //make final result array
     const result = [];
+    //find the node to traverse and start it at the root
     const nodeToTraverse = [root];
 
+
+    //while the node to traverse length is above 0
     while(nodeToTraverse.length) {
+        //keep track of the current Node. initize it to be the last value in the array
         const currentNode = nodeToTraverse.pop();
+        //push the currentNode value to the front of the result array
         result.unshift(currentNode.val);
 
+        //if the current node . left exists
         if(currentNode.left) {
+            //add it to the node to traverse array
             nodeToTraverse.push(currentNode.left);
         }
 
+        //if the current node . right exsits
         if(currentNode.right) {
+            //add is to the node to traverser array
             nodeToTraverse.push(currentNode.right);
         }
     }
 
+    //return final
     return result;
 }
