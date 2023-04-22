2b3ce44d43aa925f34ab53573c29a1029dc8bb35
Ricky Martinez <RMartinezW16@gmail.com>
2023-04-22 14:45:04 -0400

    sum multiples

--- sumMultiples.js
+++ sumMultiples.js
@@ -25,7 +25,9 @@
 
 
 var sumOfMultiples = function(n) {
+    //make a var to hold the sum
     let sum = 0;
+    
     for(let i = 1; i <= n; i++){
         if( i % 3 == 0 || i % 5 == 0 || i % 7 == 0 ) {
             sum += i
