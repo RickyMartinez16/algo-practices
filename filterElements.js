1dd7204e9fa2c09098c5f7ebb9ec3a734863dc14
Ricky Martinez <RMartinezW16@gmail.com>
2023-04-26 11:17:41 -0400

    filter elements

--- filterElements.js
+++ filterElements.js
@@ -44,3 +44,15 @@
     }
     return array
 };
+
+
+
+var filter = function(arr, fn) {
+    const res = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (fn(arr[i], i)) {
+        res.push(arr[i]);
+      }
+    }
+    return res;
+  };
