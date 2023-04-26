c8bae62d593c71c1c4a5c86e3c5ca0bbe8c6e464
Ricky Martinez <RMartinezW16@gmail.com>
2023-04-26 11:50:53 -0400

    reduce to zero steps

--- reduceToZero.js
+++ reduceToZero.js
@@ -33,3 +33,16 @@
 
 
 //------------working---------------//
+
+var numberOfSteps = function(num) {
+    let steps = 0
+    while(num > 0){
+        if(num % 2 === 0){
+            num = num / 2
+        } else {
+            num = num - 1
+        }
+        steps++
+    }
+    return steps
+};
