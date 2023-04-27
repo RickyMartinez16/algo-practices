e79ef2f82e6a3886f697d61d1f33da5f5471d658
Ricky Martinez <RMartinezW16@gmail.com>
2023-04-27 14:47:58 -0400

    sort string

--- sortingASentence.js
+++ sortingASentence.js
@@ -1,4 +1,5 @@
-// A sentence is a list of words that are separated by a single space with no leading or trailing spaces. Each word consists of lowercase and uppercase English letters.
+// A sentence is a list of words that are separated by a single space with no leading or trailing spaces. 
+//Each word consists of lowercase and uppercase English letters.
 
 // A sentence can be shuffled by appending the 1-indexed word position to each word then rearranging the words in the sentence.
 
