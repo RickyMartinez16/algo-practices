91fe43dddad8d27bcea25e1237a359900dd3dcc1
Ricky Martinez <RMartinezW16@gmail.com>
2023-05-01 13:01:54 -0400

    two string arrays

--- twoStringArrays.js
+++ twoStringArrays.js
@@ -25,3 +25,11 @@
 
 
 //---------------working----------------//
+
+
+var arrayStringsAreEqual = function(word1, word2) {
+    if(word1.join("") === word2.join("")){
+        return true
+    }
+    return false
+};
