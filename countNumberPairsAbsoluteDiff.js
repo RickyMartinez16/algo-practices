974408d8bbcd5b6cbb36218899ade739ce96a4bc
Ricky Martinez <RMartinezW16@gmail.com>
2023-05-02 12:14:02 -0400

    number pairs abs diff

--- countNumberPairsAbsoluteDiff.js
+++ countNumberPairsAbsoluteDiff.js
@@ -28,3 +28,18 @@
 // - [3,2,1,5,4]
 // - [3,2,1,5,4]
 // - [3,2,1,5,4]
+
+
+
+var countKDifference = function(nums, k) {
+    let count = 0
+
+    for(let i = 0; i < nums.length; i++){
+        for(let j = i + 1; j < nums.length; j++ ){
+            if(Math.abs(nums[i] - nums[j]) === k){
+            count++
+            }
+        }
+    }
+    return count
+};
