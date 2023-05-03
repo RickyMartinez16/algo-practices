3c2a5f5c4054120a1a827e54a70b846dc559e410
Ricky Martinez <RMartinezW16@gmail.com>
2023-05-03 12:39:33 -0400

    reveres words

--- reverseWords.js
+++ reverseWords.js
@@ -1,4 +1,5 @@
-// Given a string s, reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
+// Given a string s, reverse the order of characters in each word within a sentence while still preserving whitespace 
+//and initial word order.
 
  
 
@@ -10,3 +11,13 @@
 
 // Input: s = "God Ding"
 // Output: "doG gniD"
+
+
+var reverseWords = function(s) {
+    let words = s.split(" ");
+    let array = []
+    for(let i = 0; i < words.length; i++){
+        array.push(words[i].split("").reverse().join(""))
+    }
+    return array.join(" ")
+};
