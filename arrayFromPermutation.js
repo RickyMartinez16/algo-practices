92e24d887adf97ebc14f71edddb0437c6b786070
Ricky Martinez <RMartinezW16@gmail.com>
2023-05-05 13:01:09 -0400

    array from perm

--- arrayFromPermutation.js
+++ arrayFromPermutation.js
@@ -20,3 +20,13 @@
 // ans = [nums[nums[0]], nums[nums[1]], nums[nums[2]], nums[nums[3]], nums[nums[4]], nums[nums[5]]]
 //     = [nums[5], nums[0], nums[1], nums[2], nums[3], nums[4]]
 //     = [4,5,0,1,2,3]
+
+
+var buildArray = function(nums) {
+    let final = [];
+
+    for(let i = 0; i < nums.length; i++){
+        final.push(nums[nums[i]])
+    }
+    return final
+};
