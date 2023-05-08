3c5df86b6fa5f8adc124432d43a6d18ce254d253
Ricky Martinez <RMartinezW16@gmail.com>
2023-05-08 14:56:03 -0400

    var after ops

--- varAfterOps.js
+++ varAfterOps.js
@@ -40,7 +40,9 @@
 
 
 var finalValueAfterOperations = function(operations) {
+    //keep track of final count
     let final = 0;
+    //loop thru operations input
     for(let i = 0; i < operations.length; i++){
         if(operations[i] === "--X" || operations[i] === "X--"){
             final-= 1
