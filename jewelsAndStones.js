e780dcf21472e74a130638ccb740cbc9c7da7ce9
Ricky Martinez <RMartinezW16@gmail.com>
2023-05-09 12:51:04 -0400

    jewels and stones

--- jewelsAndStones.js
+++ jewelsAndStones.js
@@ -31,6 +31,7 @@ var numJewelsInStones = function(jewels, stones) {
         //make a matches array by checking how many times the regex returns a match of the regex from the stonees string
         const matches = stones.match(regex);
 
+        //count equals plus eqauls if matches is true its matches.length or its 0 if false
         count += matches ? matches.length : 0;
     })
 
