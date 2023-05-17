f5f113e6d6d8c25c8ef9b34058e5ead016b90532
Ricky Martinez <RMartinezW16@gmail.com>
2023-05-17 14:35:03 -0400

    rings and rods

--- ringsAndRods.js
+++ ringsAndRods.js
@@ -35,3 +35,18 @@
 // Output: 0
 // Explanation: 
 // Only one ring is given. Thus, no rods have all three colors.
+
+
+
+var countPoints = function(rings) {
+    let rods = "0123456789"
+
+    let count = 0;
+
+    for(let rod of rods){
+        if(rings.includes(`R${rod}`) && rings.includes(`G${rod}`) && rings.includes(`B${rod}`)){
+            count++
+        }
+    }
+    return count
+};
