7f8e8b3fc341c038ecde0320ac830ec294ce4830
Ricky Martinez <RMartinezW16@gmail.com>
2023-05-23 13:55:36 -0400

    occurances

--- equalOccurances.js
+++ equalOccurances.js
@@ -15,3 +15,21 @@
 // Output: false
 // Explanation: The characters that appear in s are 'a' and 'b'.
 // 'a' occurs 3 times while 'b' occurs 2 times, which is not the same number of times.
+
+var areOccurrencesEqual = function(s) {
+    let map = {}
+
+    for(let i = 0; i < s.length; i++){
+      if(!map[s[i]]){
+        map[s[i]] = 1
+      } else {
+        map[s[i]]++
+      }
+      
+    }
+    console.log(map)
+    let values = Object.values(map);
+    console.log(values)
+    let allEqual = values.every(val => val === values[0]);
+    return allEqual
+};
