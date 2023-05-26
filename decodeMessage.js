5445b0528de4bd304b6d2db1b7147fc92b3ce431
Ricky Martinez <RMartinezW16@gmail.com>
2023-05-26 12:42:19 -0400

    decode message

--- decodeMessage.js
+++ decodeMessage.js
@@ -1,25 +1,44 @@
-You are given the strings key and message, which represent a cipher key and a secret message, respectively. The steps to decode message are as follows:
+// You are given the strings key and message, which represent a cipher key and a secret message, respectively. The steps to decode message are as follows:
 
-Use the first appearance of all 26 lowercase English letters in key as the order of the substitution table.
-Align the substitution table with the regular English alphabet.
-Each letter in message is then substituted using the table.
-Spaces ' ' are transformed to themselves.
-For example, given key = "happy boy" (actual key would have at least one instance of each letter in the alphabet), we have the partial substitution table of ('h' -> 'a', 'a' -> 'b', 'p' -> 'c', 'y' -> 'd', 'b' -> 'e', 'o' -> 'f').
-Return the decoded message.
+// Use the first appearance of all 26 lowercase English letters in key as the order of the substitution table.
+// Align the substitution table with the regular English alphabet.
+// Each letter in message is then substituted using the table.
+// Spaces ' ' are transformed to themselves.
+// For example, given key = "happy boy" (actual key would have at least one instance of each letter in the alphabet), we have the partial substitution table of ('h' -> 'a', 'a' -> 'b', 'p' -> 'c', 'y' -> 'd', 'b' -> 'e', 'o' -> 'f').
+// Return the decoded message.
 
  
 
-Example 1:
+// Example 1:
 
 
-Input: key = "the quick brown fox jumps over the lazy dog", message = "vkbs bs t suepuv"
-Output: "this is a secret"
-Explanation: The diagram above shows the substitution table.
-It is obtained by taking the first appearance of each letter in "the quick brown fox jumps over the lazy dog".
-Example 2:
+// Input: key = "the quick brown fox jumps over the lazy dog", message = "vkbs bs t suepuv"
+// Output: "this is a secret"
+// Explanation: The diagram above shows the substitution table.
+// It is obtained by taking the first appearance of each letter in "the quick brown fox jumps over the lazy dog".
+// Example 2:
 
 
-Input: key = "eljuxhpwnyrdgtqkviszcfmabo", message = "zwx hnfx lqantp mnoeius ycgk vcnjrdb"
-Output: "the five boxing wizards jump quickly"
-Explanation: The diagram above shows the substitution table.
-It is obtained by taking the first appearance of each letter in "eljuxhpwnyrdgtqkviszcfmabo".
+// Input: key = "eljuxhpwnyrdgtqkviszcfmabo", message = "zwx hnfx lqantp mnoeius ycgk vcnjrdb"
+// Output: "the five boxing wizards jump quickly"
+// Explanation: The diagram above shows the substitution table.
+// It is obtained by taking the first appearance of each letter in "eljuxhpwnyrdgtqkviszcfmabo".
+
+
+var decodeMessage = function (key, message) {
+    let uniqueLetters = Array.from(new Set(key.split(' ').join('')))
+    let letterMap = new Map()
+    let result = message.split('')
+
+    for (let i = 0; i < uniqueLetters.length; i++) {
+        letterMap.set(uniqueLetters[i], String.fromCharCode(97 + i))
+    }
+
+    for (let i = 0; i < result.length; i++) {
+        if (result[i] !== ' ') {
+            result[i] = letterMap.get(result[i])
+        }
+    }
+
+    return result.join('')
+};
