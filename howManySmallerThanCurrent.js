89d141ca13e21bee9e4efe8aae05c7c7abe0fdca
Ricky Martinez <RMartinezW16@gmail.com>
2023-05-31 14:12:31 -0400

    how many smaller than current

--- howManySmallerThanCurrent.js
+++ howManySmallerThanCurrent.js
@@ -23,3 +23,21 @@
 
 // Input: nums = [7,7,7,7]
 // Output: [0,0,0,0]
+
+
+
+var smallerNumbersThanCurrent = function(nums) {
+    let array = [];
+    let count = 0
+
+    for(let i = 0; i < nums.length; i++){
+      for(let j = 0; j < nums.length; j++){
+        if(nums[i] > nums[j]){
+          count++
+        }
+      }
+      array.push(count)
+      count = 0
+    }
+    return array
+};
