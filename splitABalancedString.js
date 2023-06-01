846f83dd5294adf71720ecdf9f8e4d0e01e9dddb
Ricky Martinez <RMartinezW16@gmail.com>
2023-06-01 11:53:35 -0400

    split a balanced string

--- splitABalancedString.js
+++ splitABalancedString.js
@@ -50,3 +50,28 @@ var balancedStringSplit = function(s) {
     }
     return matches
 };
+
+
+
+//keep track of final matches
+let matches = 0;
+const stack = [];
+
+stack.push(s[0]);
+
+for (let i = 1; i < s.length; i++) {
+
+    const top = stack[stack.length - 1];
+
+    if (top !== undefined && top !== s[i]) {
+        stack.pop()
+    } else {
+        stack.push(s[i]);
+    }
+
+    if (stack.length === 0) {
+        matches += 1;
+    }
+}
+
+return matches;
