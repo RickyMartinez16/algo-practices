c8e4e323de5538927ad13cc16f1ab93ef4c26003
Ricky Martinez <RMartinezW16@gmail.com>
2023-06-05 13:12:20 -0400

    diagonal sum

--- diagonalSum.js
+++ diagonalSum.js
@@ -24,3 +24,16 @@
 
 // Input: mat = [[5]]
 // Output: 5
+
+
+var diagonalSum = function (mat) {
+    let sum = 0
+    let n = mat.length - 1
+    for (let i = 0; i <= n; i++) {
+        sum = sum + mat[i][i]
+        if (i !== (n - i)) {
+            sum = sum + mat[i][n - i]
+        }
+    }
+    return sum
+};
