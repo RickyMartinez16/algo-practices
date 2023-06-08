d0dfd619a8b21eaba9f63bb914b248abe7c2e798
Ricky Martinez <RMartinezW16@gmail.com>
2023-06-08 12:26:10 -0400

    gcd

--- greatestCommonDivisor.js
+++ greatestCommonDivisor.js
@@ -49,7 +49,7 @@ var findGCD = function(nums) {
         return a;
   }
 
-    //   In the above code, on each iteration, the smaller integer is subtracted from the larger integer. 
+    //In the above code, on each iteration, the smaller integer is subtracted from the larger integer. 
     //And the result is assigned to a variable holding the larger integer. The while loop continues iteration 
     //until both the integers become equal.
   
