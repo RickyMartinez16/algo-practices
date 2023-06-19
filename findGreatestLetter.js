44c8734381f798fe375d120131ba44371e4b1415
Ricky Martinez <RMartinezW16@gmail.com>
2023-06-19 15:16:23 -0400

    find greatest letter

--- findGreatestLetter.js
+++ findGreatestLetter.js
@@ -1,4 +1,5 @@
-// You are given an array of characters letters that is sorted in non-decreasing order, and a character target. There are at least two different characters in letters.
+// You are given an array of characters letters that is sorted in non-decreasing order, and a character target. 
+//There are at least two different characters in letters.
 
 // Return the smallest character in letters that is lexicographically greater than target. If such a character does not exist, return the first character in letters.
 
