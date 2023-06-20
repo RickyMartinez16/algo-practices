ab89f8a168e06d041bcf2717c79f42d7c1603f66
Ricky Martinez <RMartinezW16@gmail.com>
2023-06-20 15:54:43 -0400

    move zeros

--- moveZeros.js
+++ moveZeros.js
@@ -14,3 +14,17 @@
 // Output: [0]
 
 
+var moveZeroes = function(nums) {
+    let zeroCount = 0
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] === 0){
+            nums.splice(i, 1);
+            i--;
+            zeroCount++
+        }
+    }
+    for(let i = 0; i < zeroCount; i++){
+        nums.push(0)
+    }
+    return nums
+};
