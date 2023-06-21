8aa6ce74e65dc1a30c700a0b0ec04379ba102761
Ricky Martinez <RMartinezW16@gmail.com>
2023-06-21 12:02:06 -0400

    arithamthc sequence

--- arithmaticSequence.js
+++ arithmaticSequence.js
@@ -26,6 +26,7 @@ var canMakeArithmeticProgression = function(arr) {
     //find the difference yourel ooking for
     let commonDiff = arr[1] - arr[0]
 
+    //loop thru array
     for(let i = 1; i < arr.length; i++){
         if(arr[i] - arr[i - 1] != commonDiff){
             return false
