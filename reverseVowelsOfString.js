22d6eeef86e7fc9fa0809bf72c9f52d95d52a232
Ricky Martinez <RMartinezW16@gmail.com>
2023-07-06 14:24:44 -0400

    reverse vowels of string

--- reverseVowelsOfString.js
+++ reverseVowelsOfString.js
@@ -27,6 +27,7 @@ var reverseVowels = function(s) {
     for(let i = 0; i < stringSplit.length; i++){
         //check if the vowels array includes the letter youre checking from the input string (change to lwoercase)
         if(vowels.includes(stringSplit[i].toLowerCase())){
+            //if it exists in the vowels array then push it into the final array
             finalArray.push(stringSplit[i])
         }
     }
