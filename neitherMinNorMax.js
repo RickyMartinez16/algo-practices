29b60a3d9e91f111448d19113d88a213f65693aa
Ricky Martinez <RMartinezW16@gmail.com>
2023-07-09 15:16:12 -0400

    niehter min nor max

--- neitherMinNorMax.js
+++ neitherMinNorMax.js
@@ -1,4 +1,5 @@
-Given an integer array nums containing distinct positive integers, find and return any number from the array that is neither the minimum nor the maximum value in the array, or -1 if there is no such number.
+Given an integer array nums containing distinct positive integers, find and return any number 
+//from the array that is neither the minimum nor the maximum value in the array, or -1 if there is no such number.
 
 Return the selected integer.
 
@@ -13,7 +14,8 @@ Example 2:
 
 Input: nums = [1,2]
 Output: -1
-Explanation: Since there is no number in nums that is neither the maximum nor the minimum, we cannot select a number that satisfies the given condition. Therefore, there is no answer.
+Explanation: Since there is no number in nums that is neither the maximum nor the minimum, 
+we cannot select a number that satisfies the given condition. Therefore, there is no answer.
 Example 3:
 
 Input: nums = [2,1,3]
