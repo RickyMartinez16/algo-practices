d1b5192ad9b610da374c5e11173daedc6a06f7ec
Ricky Martinez <RMartinezW16@gmail.com>
2023-07-11 11:47:48 -0400

    fascinating num

--- fascinatingNum.js
+++ fascinatingNum.js
@@ -33,7 +33,7 @@ var isFascinating = function(n) {
     //join the array so its just one big number
     let finalNum = finalArray.join("")
 
-    //loop thru the big num
+    //loop thru the big num starting at 1
     for(let i = 1; i < finalNum.length; i++){
         if(finalNum.includes(0)){
             return false
