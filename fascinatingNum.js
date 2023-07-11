3fe6bb28b49c471ece20a2f95fc05560d78eb154
Ricky Martinez <RMartinezW16@gmail.com>
2023-07-11 11:48:18 -0400

    fascinating num

--- fascinatingNum.js
+++ fascinatingNum.js
@@ -38,6 +38,7 @@ var isFascinating = function(n) {
         //if the final number includes a zero its false
         if(finalNum.includes(0)){
             return false
+            //if the final number doesnt includes every number starting from 1 its false
         } else if(!finalNum.includes(i)){
             return false
         }
