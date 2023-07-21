66d5eb1c80f80b45414b203e08d0d8ece5448a3d
Ricky Martinez <RMartinezW16@gmail.com>
2023-07-21 16:13:43 -0400

    binary search

--- binarySearch.js
+++ binarySearch.js
@@ -45,6 +45,7 @@ var search = function(nums, target) {
             //you know the midpoint is to the left of the target. so move the left side closer to the right to squeeze the search
             leftSide = midPoint + 1
         } else {
+            //else you know the midpoint is greater than the target
             rightSide = midPoint - 1
         }
     }
