cf1bdafe3dd490375f0c43fdf317304090f6e22e
Ricky Martinez <RMartinezW16@gmail.com>
2023-08-01 14:08:24 -0400

    power of two

--- poeeroftwo.js
+++ poeeroftwo.js
@@ -18,3 +18,15 @@
 
 // Input: n = 3
 // Output: false
+
+
+var isPowerOfTwo = function(n) {
+    if (n == 0)
+        return 0;
+    while (n != 1) {
+        if (n%2 != 0)
+            return 0;
+        n = n/2;
+    }
+    return 1;
+};
