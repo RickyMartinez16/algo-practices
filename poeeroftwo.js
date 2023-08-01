be13ec5dde5b8883c3c52eea327823813a873f84
Ricky Martinez <RMartinezW16@gmail.com>
2023-08-01 14:10:04 -0400

    power of two

--- poeeroftwo.js
+++ poeeroftwo.js
@@ -32,6 +32,7 @@ var isPowerOfTwo = function(n) {
     while (n != 1) {
         //if the remainder of n divided by 2 does not equal 0
         if (n%2 != 0)
+        //return 0
             return 0;
         n = n/2;
     }
