903d287e7fd3795ffa2a8c9f61ff2bab739e6ffb
Ricky Martinez <RMartinezW16@gmail.com>
2023-08-04 12:26:45 -0400

    fib again

--- fibAgain.js
+++ fibAgain.js
@@ -21,3 +21,14 @@
 // Input: n = 4
 // Output: 3
 // Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
+
+
+var fib = function(n) {
+    if(n === 0) return 0
+    let fib = [0, 1]
+
+    for(let i = 2; i < n + 1 ; i++){
+        fib[i] = fib[i - 1] + fib[i - 2]
+    }
+    return fib[fib.length - 1]
+};
