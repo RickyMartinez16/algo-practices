eb8f82172d09c10bf0690d0d3968bdfcc68579d3
Ricky Martinez <RMartinezW16@gmail.com>
2023-08-07 13:27:55 -0400

    max prods

--- maxProductOfarrayEls.js
+++ maxProductOfarrayEls.js
@@ -6,7 +6,8 @@
 
 // Input: nums = [3,4,5,2]
 // Output: 12 
-// Explanation: If you choose the indices i=1 and j=2 (indexed from 0), you will get the maximum value, that is, (nums[1]-1)*(nums[2]-1) = (4-1)*(5-1) = 3*4 = 12. 
+// Explanation: If you choose the indices i=1 and j=2 (indexed from 0), you will get the maximum value, 
+//that is, (nums[1]-1)*(nums[2]-1) = (4-1)*(5-1) = 3*4 = 12. 
 // Example 2:
 
 // Input: nums = [1,5,4,5]
