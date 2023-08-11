0134eff77b1ab2cfb2a115908afc8ecd7ef034aa
Ricky Martinez <RMartinezW16@gmail.com>
2023-08-11 16:50:14 -0400

    is isomorphic

--- isIsomorphic.js
+++ isIsomorphic.js
@@ -1,27 +1,27 @@
-Given two strings s and t, determine if they are isomorphic.
+// Given two strings s and t, determine if they are isomorphic.
 
-Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+// Two strings s and t are isomorphic if the characters in s can be replaced to get t.
 
-All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+// All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
 
  
 
-Example 1:
+// Example 1:
 
-Input: s = "egg", t = "add"
-Output: true
-Example 2:
+// Input: s = "egg", t = "add"
+// Output: true
+// Example 2:
 
-Input: s = "foo", t = "bar"
-Output: false
-Example 3:
+// Input: s = "foo", t = "bar"
+// Output: false
+// Example 3:
 
-Input: s = "paper", t = "title"
-Output: true
+// Input: s = "paper", t = "title"
+// Output: true
  
 
-Constraints:
+// Constraints:
 
-1 <= s.length <= 5 * 104
-t.length == s.length
-s and t consist of any valid ascii character.
+// 1 <= s.length <= 5 * 104
+// t.length == s.length
+// s and t consist of any valid ascii character.
