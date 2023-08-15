9d9dfacec62a085f80f5213751d26f4ba575f2af
Ricky Martinez <RMartinezW16@gmail.com>
2023-08-15 13:09:32 -0400

    positive negagtive zero

--- posZeroNeg.js
+++ posZeroNeg.js
@@ -51,3 +51,25 @@
 // There are  positive numbers,  negative numbers, and  zero in the array.
 // The proportions of occurrence are positive: , negative:  and zeros: .
 
+function plusMinus(arr) {
+    let positive = 0;
+    let negative = 0;
+    let zero = 0
+    
+    let finalArray = []
+    
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] > 0){
+            positive++
+        } else if(arr[i] < 0){
+            negative++
+        } else {
+            zero++
+        }
+    }
+
+
+console.log((positive / arr.length).toFixed(6))
+console.log((negative / arr.length).toFixed(6))
+console.log((zero / arr.length).toFixed(6))
+}
