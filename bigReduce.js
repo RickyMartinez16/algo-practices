50fb8ae22a7c64046b72e555f08b3b122a5ccf19
Ricky Martinez <RMartinezW16@gmail.com>
2023-08-16 14:01:54 -0400

    big reduce

--- bigReduce.js
+++ bigReduce.js
@@ -1,35 +1,35 @@
-In this challenge, you are required to calculate and print the sum of the elements in an array, keeping in mind that some of those integers may be quite large.
+// In this challenge, you are required to calculate and print the sum of the elements in an array, keeping in mind that some of those integers may be quite large.
 
-Function Description
+// Function Description
 
-Complete the aVeryBigSum function in the editor below. It must return the sum of all array elements.
+// Complete the aVeryBigSum function in the editor below. It must return the sum of all array elements.
 
-aVeryBigSum has the following parameter(s):
+// aVeryBigSum has the following parameter(s):
 
-int ar[n]: an array of integers .
-Return
+// int ar[n]: an array of integers .
+// Return
 
-long: the sum of all array elements
-Input Format
+// long: the sum of all array elements
+// Input Format
 
-The first line of the input consists of an integer .
-The next line contains  space-separated integers contained in the array.
+// The first line of the input consists of an integer .
+// The next line contains  space-separated integers contained in the array.
 
-Output Format
+// Output Format
 
-Return the integer sum of the elements in the array.
+// Return the integer sum of the elements in the array.
 
-Constraints
+// Constraints
 
 
-Sample Input
+// Sample Input
 
-5
-1000000001 1000000002 1000000003 1000000004 1000000005
-Output
+// 5
+// 1000000001 1000000002 1000000003 1000000004 1000000005
+// Output
 
-5000000015
-Note:
+// 5000000015
+// Note:
 
-The range of the 32-bit integer is .
-When we add several integer values, the resulting sum might exceed the above range. You might need to use long int C/C++/Java to store such sums.
+// The range of the 32-bit integer is .
+// When we add several integer values, the resulting sum might exceed the above range. You might need to use long int C/C++/Java to store such sums.
