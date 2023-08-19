db8f194ab26dff81e188aac20d21e160d4d9132d
Ricky Martinez <RMartinezW16@gmail.com>
2023-08-19 12:42:35 -0400

    diag diff

--- diagDiff.js
+++ diagDiff.js
@@ -1,57 +1,57 @@
-Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+// Given a square matrix, calculate the absolute difference between the sums of its diagonals.
 
-For example, the square matrix  is shown below:
+// For example, the square matrix  is shown below:
 
-1 2 3
-4 5 6
-9 8 9  
-The left-to-right diagonal = . The right to left diagonal = . Their absolute difference is .
+// 1 2 3
+// 4 5 6
+// 9 8 9  
+// The left-to-right diagonal = . The right to left diagonal = . Their absolute difference is .
 
-Function description
+// Function description
 
-Complete the  function in the editor below.
+// Complete the  function in the editor below.
 
-diagonalDifference takes the following parameter:
+// diagonalDifference takes the following parameter:
 
-int arr[n][m]: an array of integers
-Return
+// int arr[n][m]: an array of integers
+// Return
 
-int: the absolute diagonal difference
-Input Format
+// int: the absolute diagonal difference
+// Input Format
 
-The first line contains a single integer, , the number of rows and columns in the square matrix .
-Each of the next  lines describes a row, , and consists of  space-separated integers .
+// The first line contains a single integer, , the number of rows and columns in the square matrix .
+// Each of the next  lines describes a row, , and consists of  space-separated integers .
 
-Constraints
+// Constraints
 
-Output Format
+// Output Format
 
-Return the absolute difference between the sums of the matrix's two diagonals as a single integer.
+// Return the absolute difference between the sums of the matrix's two diagonals as a single integer.
 
-Sample Input
+// Sample Input
 
-3
-11 2 4
-4 5 6
-10 8 -12
-Sample Output
+// 3
+// 11 2 4
+// 4 5 6
+// 10 8 -12
+// Sample Output
 
-15
-Explanation
+// 15
+// Explanation
 
-The primary diagonal is:
+// The primary diagonal is:
 
-11
-   5
-     -12
-Sum across the primary diagonal: 11 + 5 - 12 = 4
+// 11
+//    5
+//      -12
+// Sum across the primary diagonal: 11 + 5 - 12 = 4
 
-The secondary diagonal is:
+// The secondary diagonal is:
 
-     4
-   5
-10
-Sum across the secondary diagonal: 4 + 5 + 10 = 19
-Difference: |4 - 19| = 15
+//      4
+//    5
+// 10
+// Sum across the secondary diagonal: 4 + 5 + 10 = 19
+// Difference: |4 - 19| = 15
 
-Note: |x| is the absolute value of x
+// Note: |x| is the absolute value of x
