0d69f95f9b50f49aae30fa3dd53a7204217a4a88
Ricky Martinez <RMartinezW16@gmail.com>
2023-08-19 12:42:50 -0400

    diag diff

--- diagDiff.js
+++ diagDiff.js
@@ -55,3 +55,14 @@
 // Difference: |4 - 19| = 15
 
 // Note: |x| is the absolute value of x
+
+function diagonalDifference(arr) {
+    let diag1 = 0
+    let diag2 = 0
+    
+    for(let i = 0; i < arr.length; i++){
+        diag1 += arr[i][i]
+        diag2 += arr[i][arr.length - 1 - i]
+    }
+    return Math.abs(diag1 - diag2)
+}
