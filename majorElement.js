791fa1209a3eb9ee8d586f80e2f2d931c13d9330
Ricky Martinez <RMartinezW16@gmail.com>
2023-09-01 17:18:47 -0400

    major element

--- majorElement.js
+++ majorElement.js
@@ -19,3 +19,30 @@
 // n == nums.length
 // 1 <= n <= 5 * 104
 // -109 <= nums[i] <= 109
+
+
+var majorityElement = function(nums) {
+    let map = {}
+
+    for(let i = 0; i < nums.length; i++){
+        if(map[nums[i]]){
+            map[nums[i]]++
+        } else {
+            map[nums[i]] = 1
+        }
+    }
+
+    let maxKey = null;
+    let maxValue = -Infinity;
+
+    for (const key of Object.keys(map)) {
+        if (map[key] > maxValue) {
+            maxValue = map[key];
+            maxKey = key;
+        }
+    }
+
+    return maxKey;
+
+
+};
