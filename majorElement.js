08854a4005b65e812ddbd11cc075af5723899ec1
Ricky Martinez <RMartinezW16@gmail.com>
2023-09-01 17:19:31 -0400

    major element

--- majorElement.js
+++ majorElement.js
@@ -27,6 +27,7 @@ var majorityElement = function(nums) {
 
     //loop thru the input nums array
     for(let i = 0; i < nums.length; i++){
+        //if the map already contains the key of num[i]
         if(map[nums[i]]){
             map[nums[i]]++
         } else {
