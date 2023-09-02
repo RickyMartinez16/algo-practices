a897214042fb571b150fc0988395df7ef531df32
Ricky Martinez <RMartinezW16@gmail.com>
2023-09-02 15:28:06 -0400

    remove element

--- removingAnelement.js
+++ removingAnelement.js
@@ -44,3 +44,17 @@
 // 0 <= nums.length <= 100
 // 0 <= nums[i] <= 50
 // 0 <= val <= 100
+
+
+var removeElement = function(nums, val) {
+    let left = 0
+
+    for(let right = 0; right < nums.length; right++){
+        if(nums[right] !== val){
+            nums[left] = nums[right]
+            left++
+        }
+    }
+
+    return left
+}
