918145a3be72f759dc0517c1364e5dfe7d9166ce
Ricky Martinez <RMartinezW16@gmail.com>
2023-09-04 16:35:41 -0400

    two sum

--- 2Sum.js
+++ 2Sum.js
@@ -32,6 +32,7 @@
 var twoSum = function(nums, target) {
     // loop thru input
     for(let i = 0; i < nums.length; i++){
+        //loop thru input again to find second number
         for(let j = 1; j < nums.length; j++){
             if(nums[i] + nums[j] === target){
                 if(i != j){
