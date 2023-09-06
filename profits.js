90e76621597f39580c8c9ea89dd1fb50166c34d0
Ricky Martinez <RMartinezW16@gmail.com>
2023-09-06 12:54:59 -0400

    max profits

--- profits.js
+++ profits.js
@@ -23,3 +23,23 @@
 
 // 1 <= prices.length <= 105
 // 0 <= prices[i] <= 104
+
+
+var maxProfit = function(prices) {
+    if (prices.length <= 1) {
+        return 0;
+    }
+    
+    let minPrice = prices[0];
+    let maxProfit = 0;
+    
+    for (let i = 1; i < prices.length; i++) {
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];
+        } else {
+            maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+        }
+    }
+    
+    return maxProfit;
+};
