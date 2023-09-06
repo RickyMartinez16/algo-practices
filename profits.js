2f9bbc1a46919610b2501aa904588f78ba5096e2
Ricky Martinez <RMartinezW16@gmail.com>
2023-09-06 12:55:28 -0400

    max profits

--- profits.js
+++ profits.js
@@ -28,6 +28,7 @@
 var maxProfit = function(prices) {
     //if the length of the prices input array is eual or less than 1
     if (prices.length <= 1) {
+        //rerturn 0
         return 0;
     }
     
