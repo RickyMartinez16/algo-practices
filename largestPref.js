20b092bdc646aeb369b02c61e5a1f43395f26744
Ricky Martinez <RMartinezW16@gmail.com>
2023-09-11 14:52:37 -0400

    longest pre

--- largestPref.js
+++ largestPref.js
@@ -20,3 +20,23 @@
 // 1 <= strs.length <= 200
 // 0 <= strs[i].length <= 200
 // strs[i] consists of only lowercase English letters.
+
+
+var longestCommonPrefix = function(strs) {
+    if(strs.length < 1){
+        return ""
+    }
+
+    let prefix = strs[0]
+
+    for(let i = 1; i < strs.length; i++){
+        while(strs[i].indexOf(prefix) != 0){
+            prefix = prefix.slice(0, prefix.length - 1)
+        }
+
+        if(prefix === ""){
+            return ""
+        }
+    }
+    return prefix
+};
