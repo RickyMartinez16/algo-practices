984f8a1dbbdd2d8d5e593a503821548ccec9852e
Ricky Martinez <RMartinezW16@gmail.com>
2023-10-28 15:20:43 -0400

    power of 4

--- powOf4.js
+++ powOf4.js
@@ -24,3 +24,14 @@
  
 
 // Follow up: Could you solve it without loops/recursion?
+
+
+var isPowerOfFour = function(n) {
+    if(n <= 0) return false
+
+    while(n % 4 === 0){
+        n /= 4
+    }
+
+    return n === 1
+};
