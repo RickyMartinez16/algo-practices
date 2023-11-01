8280444d2960e0794799a9feb7675938290f8cd6
Ricky Martinez <RMartinezW16@gmail.com>
2023-11-01 12:13:20 -0400

    reverse vowels in string

--- reverseVowelsInaString.js
+++ reverseVowelsInaString.js
@@ -1,20 +1,20 @@
-Given a string s, reverse only all the vowels in the string and return it.
+// Given a string s, reverse only all the vowels in the string and return it.
 
-The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
+// The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
 
  
 
-Example 1:
+// Example 1:
 
-Input: s = "hello"
-Output: "holle"
-Example 2:
+// Input: s = "hello"
+// Output: "holle"
+// Example 2:
 
-Input: s = "leetcode"
-Output: "leotcede"
+// Input: s = "leetcode"
+// Output: "leotcede"
  
 
-Constraints:
+// Constraints:
 
-1 <= s.length <= 3 * 105
-s consist of printable ASCII characters.
+// 1 <= s.length <= 3 * 105
+// s consist of printable ASCII characters.
