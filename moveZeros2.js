696b95c0aefaa092c41f05c790d988527b36d10c
Ricky Martinez <RMartinezW16@gmail.com>
2023-11-06 16:44:49 -0500

    move zeros

--- moveZeros2.js
+++ moveZeros2.js
@@ -19,3 +19,19 @@
 
 // 1 <= nums.length <= 104
 // -231 <= nums[i] <= 231 - 1
+
+
+var moveZeroes = function(nums) {
+    if(!nums.includes(0)) return nums
+
+    let nonZeroIndex = 0;
+
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] !== 0){
+            [nums[i], nums[nonZeroIndex]] = [nums[nonZeroIndex], nums[i]];
+            nonZeroIndex++
+        }
+    }
+
+    return nums
+};
