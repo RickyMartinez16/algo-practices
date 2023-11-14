2dc8abafa077e27fd6dbc983570911cc56a59cdb
Ricky Martinez <RMartinezW16@gmail.com>
2023-11-14 15:03:53 -0500

    first unique char

--- uniqueCharStringFirst.js
+++ uniqueCharStringFirst.js
@@ -21,3 +21,27 @@
 
 // 1 <= s.length <= 105
 // s consists of only lowercase English letters.
+
+
+var firstUniqChar = function(s) {
+
+    let map = {}
+
+    for(let i = 0; i < s.length; i++){
+        if(map[s[i]]){
+            map[s[i]]++
+        } else {
+            map[s[i]] = 1
+        }
+    }
+    
+
+    for(let i = 0; i < s.length; i++){
+        if(map[s[i]] === 1){
+            return i
+        }
+    }
+    return -1
+
+
+};
