2933b7696ba76747998a40f5e4182eba6f4b1518
Ricky Martinez <RMartinezW16@gmail.com>
2023-12-01 17:19:37 -0500

    fuzz buss

--- fixxBuzz.js
+++ fixxBuzz.js
@@ -33,6 +33,7 @@ var fizzBuzz = function(n) {
     for(let i = 1; i <= n; i++){
         //if remainder of 1 / 3 is 0 AND remainder of 1 / 5 is 0
         if(i % 3 === 0 && i % 5 === 0){
+            //push fizzbuzz
             array.push("FizzBuzz")
         } else if(i % 3 === 0){
             array.push("Fizz")
