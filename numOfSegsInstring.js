c4ecb14663a417581d184113e2b48bad5645c624
Ricky Martinez <RMartinezW16@gmail.com>
2023-12-02 14:36:39 -0500

    num of strings in array

--- numOfSegsInstring.js
+++ numOfSegsInstring.js
@@ -1,22 +1,22 @@
-Given a string s, return the number of segments in the string.
+// Given a string s, return the number of segments in the string.
 
-A segment is defined to be a contiguous sequence of non-space characters.
+// A segment is defined to be a contiguous sequence of non-space characters.
 
  
 
-Example 1:
+// Example 1:
 
-Input: s = "Hello, my name is John"
-Output: 5
-Explanation: The five segments are ["Hello,", "my", "name", "is", "John"]
-Example 2:
+// Input: s = "Hello, my name is John"
+// Output: 5
+// Explanation: The five segments are ["Hello,", "my", "name", "is", "John"]
+// Example 2:
 
-Input: s = "Hello"
-Output: 1
+// Input: s = "Hello"
+// Output: 1
  
 
-Constraints:
+// Constraints:
 
-0 <= s.length <= 300
-s consists of lowercase and uppercase English letters, digits, or one of the following characters "!@#$%^&*()_+-=',.:".
-The only space character in s is ' '.
+// 0 <= s.length <= 300
+// s consists of lowercase and uppercase English letters, digits, or one of the following characters "!@#$%^&*()_+-=',.:".
+// The only space character in s is ' '.
