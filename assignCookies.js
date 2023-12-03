586701d411be85941863dbd189fbe9223bfaef3a
Ricky Martinez <RMartinezW16@gmail.com>
2023-12-03 16:04:32 -0500

    assign cookies

---
+++ assignCookies.js
@@ -0,0 +1,26 @@
+var findContentChildren = function(g, s) {
+    // Sort the arrays in ascending order
+    g.sort((a, b) => a - b);
+    s.sort((a, b) => a - b);
+
+    let kids = 0;
+
+    for (let i = 0; i < g.length; i++) {
+        let j = 0;
+        while (j < s.length && s[j] < g[i]) {
+            // Skip cookies that are too small for the current child
+            j++;
+        }
+
+        if (j < s.length) {
+            // Assign the smallest available cookie to the current child
+            kids++;
+            s.splice(j, 1); // Remove the assigned cookie from the array
+        } else {
+            // No more cookies available for the current child
+            break;
+        }
+    }
+
+    return kids;
+};
